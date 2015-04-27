e052a82efe96a79a058fee4e39d527b34ad45d73
Aleksandr Arofikin <sashaaro@gmail.com>
2015-04-27 12:22:39 +0300

    download file name

--- src/MBH/Bundle/PackageBundle/Resources/public/js/documents/001.js
+++ src/MBH/Bundle/PackageBundle/Resources/public/js/documents/001.js
@@ -2,13 +2,13 @@
  * Created by mb on 23.04.15.
  */
 (function($){
-    $(".remove-package-document").on("click", function(){
+    /*$(".remove-package-document").on("click", function(){
         var $this = $(this);
         $this.addClass("disabled").children("i.fa-trash-o").removeClass("fa-trash-o").addClass("fa-spinner fa-spin");
 
         $.ajax({
             url: Routing.generate('package_remove_document', {id: $this.data('package')}),
-            data: {filename : $this.data('filename')},
+            data: {docname : $this.data('docname')},
             method: "post",
             success: function (data) {
                 if(data.success){
@@ -19,14 +19,23 @@
             },
             dataType: 'json'
         });
-    })
+    })*/
 
     $('#documents-table').dataTable({
-        paging: false,
+        //paging: false,
         pageLength: 10,
         order: [[4, 'desc']],
-        info: false,
+        info: true,
         stateSave: true,
+
+
+        /*"sPaginationType": "full_numbers",
+        "bFilter": false,
+        "bSearchable":false,
+        "bInfo":false,*/
+        "bLengthChange": false,
+        //"bPaginate": false,
+
         language: {
             "sProcessing": "Подождите...",
             "sLengthMenu": "Показать _MENU_ записей",
