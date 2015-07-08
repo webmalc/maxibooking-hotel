5200ac7b557eb3f231ba7b395dfded3ac0e84cc3
Aleksandr Arofikin <sashaaro@gmail.com>
2015-07-08 15:18:07 +0300

    combine menu again

--- src/MBH/Bundle/PackageBundle/Menu/Builder.php
+++ src/MBH/Bundle/PackageBundle/Menu/Builder.php
@@ -3,6 +3,7 @@
 namespace MBH\Bundle\PackageBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Knp\Menu\MenuItem;
 use MBH\Bundle\PackageBundle\Component\DocumentTemplateGenerator\DocumentTemplateGeneratorFactory;
 use MBH\Bundle\PackageBundle\Document\Package;
 use Symfony\Component\DependencyInjection\ContainerAware;
@@ -16,28 +17,92 @@ class Builder extends ContainerAware
     public function templateDocuments(FactoryInterface $factory, array $options)
     {
         $package = $options['package'];
+        $searchQuery = $options['searchQuery'];
         if(!$package instanceof Package) {
             throw new \InvalidArgumentException();
         }
 
-        $menu = $factory->createItem('Генерация документов');
-        $menu->setAttribute('class', 'dropdown-menu');
+        $menu = $factory->createItem('Docs Generation');
+        $translator = $this->container->get('translator');
 
+
+        $menu->setAttribute('class', 'dropdown-menu');
         $menu->setChildrenAttributes([
             'class' => 'dropdown-menu',
             'role' => 'menu',
             'data-id' => $package->getId()
         ]);
 
-        $types = DocumentTemplateGeneratorFactory::getAvailableTypes();
-        $translator = $this->container->get('translator');
-
-        /*$menu
-            ->addChild('presentation', [
+        $menu
+            ->addChild('Docs header', [
                 'label' => $translator->trans('package.actions.docs', [], 'MBHPackageBundle')
             ])
-            ->setAttribute('class', 'dropdown-header');*/
+            ->setAttribute('dropdown_header', true);
 
+        $this->addDocumentTemplateItems($menu, $package);
+
+        $menu
+            ->addChild('Search Header', [
+                'label' => $translator->trans('package.actions.search', [], 'MBHPackageBundle')
+            ])
+            ->setAttributes([
+                'divider_prepend' => true,
+                'dropdown_header' => true
+            ]);
+
+        $menu
+            ->addChild('Package search', [
+                'uri' => $this->container->get('router')->generate('package_search').'#'.twig_urlencode_filter(['s' => $searchQuery]),
+                'label' => $translator->trans('package.actions.find_similar', [], 'MBHPackageBundle')
+            ])
+            ->setAttribute('icon', 'fa fa-search');
+
+        $menu
+            ->addChild('Order search ', [
+                'route' => 'package_search',
+                'routeParameters' => ['order' => $package->getOrder()->getId()],
+                'label' => $translator->trans('order.package.add', [], 'MBHPackageBundle')
+            ])
+            ->setAttributes([
+                'icon' => 'fa fa-search',
+                'divider_append' => true,
+                'level' => 2,
+            ]);
+
+        $menu
+            ->addChild('Delete Header', [
+                'label' => $translator->trans('package.actions.delete', [], 'MBHPackageBundle')
+            ])
+            ->setAttribute('dropdown_header', true);
+
+        $menu
+            ->addChild('Delete', [
+                'route' => 'package_delete',
+                'routeParameters' => ['id' => $package->getId()],
+                'label' => $translator->trans('package.actions.delete', [], 'MBHPackageBundle')
+            ])
+            ->setAttributes([
+                'icon' => 'fa fa-trash-o',
+                'class' => 'delete-link'
+            ]);
+        $menu
+            ->addChild('Order delete', [
+                'route' => 'package_search',
+                'routeParameters' => ['order' => $package->getOrder()->getId()],
+                'label' => $translator->trans('order.navbar.delete_order', [], 'MBHPackageBundle')
+            ])
+            ->setAttributes([
+                'icon' => 'fa fa-trash-o',
+                'class' => 'delete-link'
+            ]);
+
+        return $menu;
+    }
+
+    private function addDocumentTemplateItems(MenuItem $menu, Package $package)
+    {
+        $translator = $this->container->get('translator');
+        $types = DocumentTemplateGeneratorFactory::getAvailableTypes();
         $generateFactory = new DocumentTemplateGeneratorFactory($this->container);
 
         foreach($types as $type) {
@@ -49,7 +114,7 @@ class Builder extends ContainerAware
                 $options['route'] = 'package_pdf';
                 $options['routeParameters'] = ['type' => $type, 'id' => $package->getId()];
             }
-            $item = $menu->addChild($type, $options)->setAttribute('icon', 'fa fa-print');
+            $item = $menu->addChild($type, $options);
             if($hasForm) {
                 $item->setLinkAttributes([
                     'data-type' => $type,
@@ -57,9 +122,9 @@ class Builder extends ContainerAware
                     'data-target' => '#template-document-modal'
                 ]);
             }
-            $item->setLinkAttribute('target', '_blank');
+            $item
+                ->setAttribute('icon', 'fa fa-print')
+                ->setLinkAttribute('target', '_blank');
         }
-
-        return $menu;
     }
 }
