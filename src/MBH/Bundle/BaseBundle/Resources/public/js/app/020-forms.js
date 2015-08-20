fcce6cb9cbae7e1d83f16ee7357737c76e52eac9
Aleksandr Arofikin <sashaaro@gmail.com>
2015-08-20 10:53:51 +0300

    tagsSelectWidget

--- src/MBH/Bundle/BaseBundle/Resources/public/js/app/020-forms.js
+++ src/MBH/Bundle/BaseBundle/Resources/public/js/app/020-forms.js
@@ -229,6 +229,61 @@ var docReadyForms = function () {
     $('input[type=file]').bootstrapFileInput();
 };
 
+
+/**
+ * tagsSelectWidget
+ * @author Aleksandr Arofikin <sashaaro@gmail.com>
+ */
+(function ($) {
+    var methods = {
+        init : function (options) {
+            return this.each(function () {
+                var $widget = $(this);
+
+                if ($widget.is("select")) {
+                    $widget = $widget.wrap('<div class="tags-select-widget"></div>').closest('.tags-select-widget').prepend('<div class="list"></div>');
+                }
+
+                $widget.find('select').on('change', function () {
+                    var $this = $(this),
+                        value = $this.val();
+                    if (value) {
+                        var text = $this.find('option[value=' + $this.val() + ']').text();
+                        $widget.find('.list').append('<div class="btn btn-xs btn-default">' + text + ' <i class="fa fa-times"></i><div>');
+                        $this.val('');
+                    }
+                });
+                $widget.on('click', '.list .fa-times', function () {
+                    $(this).parent().remove();
+                });
+            });
+        },
+        clear : function () {
+            return this.each(function () {
+                $(this).find('.list').empty();
+            });
+        },
+        update : function (content) {}
+    };
+
+    $.fn.tagsSelectWidget = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Метод с именем ' +  method + ' не существует');
+        }
+    };
+})(window.jQuery);
+
+$('.tags-select-widget').tagsSelectWidget();
+$('.tags-select-input-widget').tagsSelectWidget();
+
+
+
+
+
 $(document).ready(function () {
     'use strict';
 
