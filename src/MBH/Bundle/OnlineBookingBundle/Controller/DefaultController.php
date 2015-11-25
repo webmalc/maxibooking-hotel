991c2b6d8f5b0758f372ab866de6f156816a0ae5
Aleksandr Arofikin <sashaaro@gmail.com>
2015-11-25 15:09:51 +0300

    1

--- src/MBH/Bundle/OnlineBookingBundle/Controller/DefaultController.php
+++ src/MBH/Bundle/OnlineBookingBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use MBH\Bundle\BaseBundle\Controller\BaseController;
 use MBH\Bundle\HotelBundle\Model\RoomTypeRepositoryInterface;
 use MBH\Bundle\PackageBundle\Lib\SearchQuery;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -15,15 +16,6 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends BaseController
 {
     /**
-     * Index template
-     * @Route("/", name="online_booking_index")
-     */
-    public function indexAction()
-    {
-        return $this->render('MBHOnlineBookingBundle:Default:index.html.twig', array('name' => '1'));
-    }
-
-    /**
      * @Route("/form", name="online_booking_form")
      */
     public function formAction()
@@ -32,18 +24,40 @@ class DefaultController extends BaseController
         /** @var RoomTypeRepositoryInterface $roomTypeRepository */
         $roomTypeRepository = $this->get('mbh.hotel.room_type_manager')->getRepository();
         $roomTypes = $roomTypeRepository->findAll();
+
+        $requestSearchUrl = $this->getParameter('online_booking')['request_search_url'];
+
         return $this->render('MBHOnlineBookingBundle:Default:form.html.twig', [
             'hotels' => $hotels,
-            'roomTypes' => $roomTypes
+            'roomTypes' => $roomTypes,
+            'requestSearchUrl' => $requestSearchUrl
         ]);
     }
 
+    /**
+     * @Route("/search", name="online_booking_search")
+     * @Template()
+     */
     public function searchAction(Request $request)
     {
         $searchQuery = new SearchQuery();
-        //$searchQuery->
-        $searchResaults = $this->get('mbh.package.search')->search($searchQuery);
+        $hotel = $this->dm->getRepository('MBHHotelBundle:Hotel')->find($request->get('hotel'));
+        if($hotel) {
+            $searchQuery->addHotel($hotel);
+        }
+        if($request->get('roomType')) {
+            $searchQuery->addRoomType($request->get('roomType'));
+        }
+        $helper = $this->get('mbh.helper');
+        $searchQuery->begin = $helper->getDateFromString($request->get('begin'));
+        $searchQuery->end = $helper->getDateFromString($request->get('end'));
+        $searchQuery->adults = $request->get('adults');
+        $searchQuery->children = $request->get('children');
 
-        return [];
+        $searchResults = $this->get('mbh.package.search')->search($searchQuery);
+
+        return [
+            'searchResults' => $searchResults
+        ];
     }
 }
