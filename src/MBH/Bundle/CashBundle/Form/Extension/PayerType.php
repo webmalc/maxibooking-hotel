e21974ee3d94d68afc8bdc0125321726cd520e53
Aleksandr Arofikin <sashaaro@gmail.com>
2015-12-07 17:35:06 +0300

    1

--- src/MBH/Bundle/CashBundle/Form/Extension/PayerType.php
+++ src/MBH/Bundle/CashBundle/Form/Extension/PayerType.php
@@ -26,16 +26,31 @@ class PayerType extends AbstractType
         foreach ($payers as $payer) {
             $text = $payer->getName();
             if ($payer instanceof Organization) {
-                $prefix = 'org';
                 $text .= ' (ИНН ' . $payer->getInn() . ') ' . $payer->getDirectorFio();
             } elseif ($payer instanceof Tourist) {
-                $prefix = 'tourist';
                 $text .= $payer->getBirthday() ? ' ' . $payer->getBirthday()->format('d.m.Y') : '';
             } else {
                 throw new \Exception();
             }
 
-            $result[$prefix . '_' . $payer->getId()] = $text;
+            $result[$payer->getId()] = $text;
+        }
+        return $result;
+    }
+
+    protected function choicesAttrFromPayers($payers)
+    {
+        $result = [];
+        foreach ($payers as $payer) {
+            if ($payer instanceof Organization) {
+                $prefix = 'organization';
+            } elseif ($payer instanceof Tourist) {
+                $prefix = 'tourist';
+            } else {
+                throw new \Exception();
+            }
+
+            $result[$payer->getId()] = ['data-type' => $prefix];
         }
         return $result;
     }
@@ -43,6 +58,31 @@ class PayerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
+
+        $uniqud = uniqid();
+
+        $options['ajax'] = 0;
+        $tourist = new Tourist();
+        $tourist->setFirstName('11');
+
+        $reflectionClass = new \ReflectionClass(Tourist::class);
+        $prop = $reflectionClass->getProperty('id');
+        $prop->setAccessible(true);
+        $prop->setValue($tourist, '1111');
+
+        $tourist->setBirthday(new \DateTime());
+        $organization = new Organization();
+        $organization->setName('22');
+        $organization->setInn('22312');
+
+        $reflectionClass = new \ReflectionClass(Organization::class);
+        $prop = $reflectionClass->getProperty('id');
+        $prop->setAccessible(true);
+        $prop->setValue($organization, '2222');
+        $options['payers'] = [$tourist, $organization];
+
+        /*dump($this->choicesFromPayers($options['payers']));
+        dump($this->choicesAttrFromPayers($options['payers']));*/
 
         $builder
             ->add('select', 'choice', [
@@ -52,21 +92,31 @@ class PayerType extends AbstractType
                 'data' => $options['payer'] ? $options['payer'] : null,
                 'group' => $options['groupName'],
                 'choices' => $this->choicesFromPayers($options['payers']),
+                'choice_attr' => $this->choicesAttrFromPayers($options['payers']),
+                'empty_value' => '',
                 'attr' => [
                     'placeholder' => 'form.cashDocumentType.placeholder_fio',
-                    'class' => 'select-payer plain-html',
+                    'class' => 'select-payer'.($options['ajax'] ? ' plain-html' : ''),
                     'style' => 'min-width: 500px',
-                    'data-ajax' => (int) $options['ajax']
+                    'data-ajax' => (int) $options['ajax'],
+                    'data-uniqud' => $uniqud,
                 ],
-                'empty_value' => ''
             ])
             ->add('organization', 'hidden', [
                 'required' => false,
                 'mapped' => false,
+                'attr' => [
+                    'class' => 'organization-hidden',
+                    'data-uniqud' => $uniqud,
+                ]
             ])
             ->add('tourist', 'hidden', [
                 'required' => false,
                 'mapped' => false,
+                'attr' => [
+                    'class' => 'tourist-hidden',
+                    'data-uniqud' => $uniqud,
+                ]
             ])
         ;
     }
--- src/MBH/Bundle/CashBundle/Resources/public/js/payer-form-type.js
+++ src/MBH/Bundle/CashBundle/Resources/public/js/payer-form-type.js
@@ -1,31 +1,65 @@
 /* global $, document, window */
 $(document).on('ready', function() {
-    $('.select-payer').each(function() {
+    $('select.select-payer').each(function() {
         var $select = $(this);
         var isAjax = $select.data('ajax');
-        var select2Options = {};
+
         if(isAjax) {
-            select2Options = {
-                minimumInputLength: 3,
-                allowClear: true,
-                placeholder: 'Выберите гостя',
-                ajax: {
-                    url: Routing.generate('get_payers'),
-                    dataType: 'json',
-                    data: function (params) {
-                        return {
-                            query: params.term // search term
-                        };
-                    },
-                    processResults: function (data) {
-                        console.log(data);
-                        return {results: [{id: 1, text: 12}]};
-                        return data;
-                    }
+            var select2Options = {
+                //placeholder: 'Выберите плательщика',
+            };
+            select2Options.allowClear = true;
+            select2Options.minimumInputLength = 3;
+            select2Options.dropdownCssClass=  "bigdrop";
+            select2Options.ajax = {
+                url: Routing.generate('get_payers'),
+                dataType: 'json',
+                data: function (params) {
+                    return {
+                        query: params.term // search term
+                    };
                 },
-                dropdownCssClass: "bigdrop"
+                processResults: function (data) {
+                    console.log(data);
+                    return {results: [{id: 1, text: 12}]};
+                    return data;
+                }
+            };
+            $select.select2(select2Options);
+            console.log(1);
+        }
+
+        var uniqud = $select.data('uniqud');
+        var $touristInput = $('.tourist-hidden[data-uniqud='+uniqud+']');
+        var $organizationInput = $('.organization-hidden[data-uniqud='+uniqud+']');
+
+        var typeInputs = {
+            tourist: $touristInput,
+            organization: $organizationInput
+        }
+
+        for(var type in typeInputs) {
+            var $input = typeInputs[type];
+            if($input.val()) {
+                if(isAjax) {
+                    $select.mbhSelect2OptionsFilter(function(){}, '<option selected value="'+1+'" data-type="'+type+'">1</option>');
+                } else {
+                    $select.find('option[value='+value+']').attr('selected', 'selected');
+                }
+                $select.trigger('change');
             }
         }
-        $select.select2(select2Options)
+
+        $select.on('change', function() {
+            var value = $select.val();
+            $touristInput.val('');
+            $organizationInput.val('');
+            if(value) {
+                var $selectedOption = $select.find('option[value=' + value + ']');
+                var type = $selectedOption.data('type');
+                console.log(type);
+                typeInputs[type].val(value);
+            }
+        })
     })
 });
