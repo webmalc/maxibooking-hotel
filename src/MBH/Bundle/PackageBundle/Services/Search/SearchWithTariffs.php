8feabb767806974fe4e710ebdcfaa274beace099
webmalc <webmalc@gmail.com>
2016-02-05 13:44:16 +0300

    Search sorting by price asc

--- src/MBH/Bundle/PackageBundle/Services/Search/SearchWithTariffs.php
+++ src/MBH/Bundle/PackageBundle/Services/Search/SearchWithTariffs.php
@@ -63,9 +63,60 @@ class SearchWithTariffs implements SearchInterface
                     'results' => []
                 ];
             }
+
             $groupedResult[$roomType->getId()]['results'][] = $row;
         }
 
+        return $this->sort($groupedResult);
+    }
+
+    public function sort(array $groupedResult)
+    {
+        // sort RoomTypes
+        usort($groupedResult, function ($prev, $next) {
+            if (empty($prev['results'][0])) {
+                return 1;
+            }
+            if (empty($next['results'][0])) {
+                return -1;
+            }
+            $prevFirstPrices = array_values($prev['results'][0]->getPrices());
+            $nextFirstPrices = array_values($next['results'][0]->getPrices());
+
+            if (empty($prevFirstPrices[0])) {
+                return 1;
+            }
+            if (empty($nextFirstPrices[0])) {
+                return -1;
+            }
+            if ($prevFirstPrices[0] == $nextFirstPrices[0]) {
+                return 0;
+            }
+
+            return ($prevFirstPrices[0] < $nextFirstPrices[0]) ? -1 : 1;
+        });
+
+        // sort tariffs
+        foreach ($groupedResult as $key => $roomTypeData) {
+            usort($groupedResult[$key]['results'], function ($prev, $next) {
+
+                $prevFirstPrices = array_values($prev->getPrices());
+                $nextFirstPrices = array_values($next->getPrices());
+
+                if (empty($prevFirstPrices[0])) {
+                    return 1;
+                }
+                if (empty($nextFirstPrices[0])) {
+                    return -1;
+                }
+                if ($prevFirstPrices[0] == $nextFirstPrices[0]) {
+                    return 0;
+                }
+
+                return ($prevFirstPrices[0] < $nextFirstPrices[0]) ? -1 : 1;
+            });
+
+        }
 
         return $groupedResult;
     }
