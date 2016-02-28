bfb6152361185334719fa4db8850f82820a07392
webmalc <webmalc@gmail.com>
2016-02-28 19:48:55 +0300

    DocumentTemplate generation

--- src/MBH/Bundle/ClientBundle/Controller/DocumentTemplateController.php
+++ src/MBH/Bundle/ClientBundle/Controller/DocumentTemplateController.php
@@ -7,6 +7,7 @@ use MBH\Bundle\BaseBundle\Controller\BaseController;
 use MBH\Bundle\ClientBundle\Document\DocumentTemplate;
 use MBH\Bundle\ClientBundle\Form\DocumentTemplateType;
 use MBH\Bundle\ClientBundle\Service\TemplateFormatter;
+use MBH\Bundle\PackageBundle\Document\Package;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -32,7 +33,9 @@ class DocumentTemplateController extends BaseController
      */
     public function indexAction()
     {
-        $entities = $this->dm->getRepository('MBHClientBundle:DocumentTemplate')->findAll();
+        $entities = $this->dm
+            ->getRepository('MBHClientBundle:DocumentTemplate')
+            ->findBy([], ['title' => 'asc']);
 
         return [
             'entities' => $entities,
@@ -107,6 +110,40 @@ class DocumentTemplateController extends BaseController
         $entity = $this->dm->getRepository('MBHPackageBundle:Order')->findOneBy([]);
         $html = (new TemplateFormatter())->prepareHtml($documentTemplate, $entity);
         $content = $this->get('knp_snappy.pdf')->getOutputFromHtml($html);
+        return new Response($content, 200, [
+            'Content-Type' => 'application/pdf'
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}/order/{packageId}", name="document_templates_show")
+     * @Method("GET")
+     * @Security("is_granted('ROLE_DOCUMENTS_GENERATOR')")
+     * @ParamConverter(class="MBHClientBundle:DocumentTemplate")
+     * @ParamConverter("package", class="MBHPackageBundle:Package", options={"id" = "packageId"})
+     * @return Response
+     * @param DocumentTemplate $doc
+     * @param Package $package
+     */
+    public function showAction(DocumentTemplate $doc, Package $package)
+    {
+        $env = new \Twig_Environment(new \Twig_Loader_String());
+
+        $order = $package->getOrder();
+        $hotel = $doc->getHotel() ? $doc->getHotel() : $package->getRoomType()->getHotel();
+        $organization = $doc->getOrganization() ? $doc->getOrganization() : $hotel->getOrganization();
+        $content = $this->get('knp_snappy.pdf')->getOutputFromHtml(
+            $env->render(
+                $doc->getContent(),
+                [
+                    'package' => $package,
+                    'order' => $order,
+                    'hotel' => $hotel,
+                    'payer' => $order->getPayer(),
+                    'organization' => $organization
+                ]
+            )
+        );
         return new Response($content, 200, [
             'Content-Type' => 'application/pdf'
         ]);
