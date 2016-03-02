a3e3b4c20e1862840f311e286ab8a81c0d01971f
webmalc <webmalc@gmail.com>
2016-03-02 12:43:20 +0300

    Search fix

--- src/MBH/Bundle/PackageBundle/Services/Search/Search.php
+++ src/MBH/Bundle/PackageBundle/Services/Search/Search.php
@@ -89,11 +89,6 @@ class Search implements SearchInterface
         $beforeArrival = $today->diff($query->begin)->format('%a');
         $helper = $this->container->get('mbh.helper');
 
-        if (!PromotionConditionFactory::checkConditions(
-            $query->tariff, $duration, $query->adults, $query->children
-        )) {
-            return $results;
-        }
 
         //roomTypes
         if (empty($query->roomTypes)) {
@@ -274,6 +269,12 @@ class Search implements SearchInterface
             }
             // check hotel permission
             if (!$query->isOnline && !$this->container->get('mbh.hotel.selector')->checkPermissions($hotel)) {
+                continue;
+            }
+
+            if (!PromotionConditionFactory::checkConditions(
+                $tariff, $duration, $query->adults, $query->children
+            )) {
                 continue;
             }
 
