aa9aab823e7db8daf59ef92e0dfc869f644f431c
webmalc <webmalc@gmail.com>
2016-03-30 16:13:28 +0300

    Update script for az

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
-SERVER='root@128.199.55.176'
+SERVER='root@176.112.204.203'
 GREEN='\e[0;32m'
 RED='\e[0;31m'
 NC='\e[0m'
 
-if [[ -z "$1" ]]; then
-	echo -e "${RED}Error: path=false! Usage: scripts/deploy.sh last.mbf [update] ${NC}"
-	exit
-fi
-
-if [[ $2 == 'new' ]]; then
-    IGNORE='ignore_new.txt'
-else
-    IGNORE='ignore_update.txt'
-fi
-
-if [[ -z "$3" && $2 == 'new' ]]; then
-    echo -e "${RED}Error: path=false! Usage: scripts/deploy.sh last.mbf new password ${NC}"
-	exit
-fi
-
-if [[ $4 == 'demo' ]]; then
-    SERVER='root@95.85.3.188'
-fi
-
-
+NAME='az'
+IGNORE='ignore_update.txt'
 DB_USER="${1////_}"
-FOLDER='/var/www/'$1'/'
-APC=$FOLDER'bin/console mbh:demo:apc --name='$1
+FOLDER='/var/www/'$NAME'/'
+APC=$FOLDER'bin/console mbh:demo:apc --name='$NAME
 CACHE='rm -rf '$FOLDER'var/cache/*'
 PROXIES=$FOLDER'bin/console doctrine:mongodb:generate:proxies'
 HYDRATORS=$FOLDER'bin/console doctrine:mongodb:generate:hydrators'
@@ -42,23 +23,6 @@ echo -e "${GREEN}Start rsync${NC}"
 
 rsync -avz --delete --exclude-from=scripts/$IGNORE * -e ssh $SERVER:$FOLDER
 
-if [[ $2 == 'new' ]]; then
-
-    echo -e "${GREEN}New database${NC}"
-    ssh $SERVER '/root/scripts/mongo_user.sh '$3' '$DB_USER
-
-    echo -e "${GREEN}New nginx server${NC}"
-    ssh $SERVER '/root/scripts/nginx.sh '$DB_USER
-
-    echo -e "${GREEN}Move parameters.yml${NC}"
-    ssh $SERVER $MOVE_PARAMS
-
-    echo -e "${GREEN}New permisssion${NC}"
-    ssh $SERVER 'chmod -R 777 '$FOLDER'protectedUpload'
-    ssh $SERVER 'chmod -R 777 '$FOLDER'web/upload'
-    ssh $SERVER 'chmod -R 777 '$FOLDER'web/media'
-fi
-
 echo -e "${GREEN}Start mbh:demo:apc${NC}"
 ssh $SERVER $APC
 
@@ -72,7 +36,7 @@ ssh $SERVER $CACHE
 echo -e "${GREEN}Start doctrine:mongodb:generate:hydrators${NC}"
 ssh $SERVER $HYDRATORS
 
-echo -e "${GREEN}Start octrine:mongodb:generate:proxies${NC}"
+echo -e "${GREEN}Start doctrine:mongodb:generate:proxies${NC}"
 ssh $SERVER $PROXIES
 
 echo -e "${GREEN}Start fos:dump${NC}"
@@ -86,15 +50,6 @@ ssh $SERVER $ASSESTIC
 
 echo -e "${GREEN}Start doctrine:mongodb:schema:create${NC}"
 ssh $SERVER $DB
-
-if [[ $2 == 'new' ]]; then
-    echo -e "${GREEN}Upload fixtures${NC}"
-    ssh $SERVER $FOLDER'bin/console mbh:base:fixtures'
-    ssh $SERVER $FOLDER'bin/console mbh:city:load'
-    ssh $SERVER $FOLDER'bin/console mbh:vega:import'
-    ssh $SERVER $FOLDER'bin/console mbh:currency:load'
-    ssh $SERVER $FOLDER'bin/console doctrine:mongodb:fixtures:load --append'
-fi
 
 echo -e "${GREEN}Start clear:cache${NC}"
 ssh $SERVER $CACHE
