32420c3e420d1b14f452a4fa8a2ff0d6c4b2b1e8
webmalc <webmalc@gmail.com>
2016-04-14 15:19:59 +0300

    Tariff extend workflow #2

--- src/MBH/Bundle/PriceBundle/Form/TariffInheritanceType.php
+++ src/MBH/Bundle/PriceBundle/Form/TariffInheritanceType.php
@@ -16,38 +16,42 @@ class TariffInheritanceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
+        $group = 'Наследование из тарифа <' . $options['parent'] . '>';
+
         $builder
-            ->add('services', 'document', [
-                'label' => 'Доступные услуги',
-                'group' => 'Общая информация',
+            ->add('inheritPrices', 'checkbox', [
+                'label' => 'Цены',
+                'group' => $group,
                 'required' => false,
-                'attr' => ['data-placeholder' => 'Все услуги'],
-                'class' => 'MBH\Bundle\PriceBundle\Document\Service',
-                'choices' => $options['services'],
-                'multiple' => true
+                'help' => 'Наследовать ли цены из родительского тарифа'
             ])
-            ->add('defaultServices', 'collection', [
-                'label' => 'Услуги по умолчанию',
-                'group' => 'Общая информация',
+            ->add('inheritRestrictions', 'checkbox', [
+                'label' => 'Условия и ограничения',
+                'group' => $group,
                 'required' => false,
-                'type' => new TariffServiceType($options['services']),
-                'allow_add' => true,
-                'allow_delete' => true,
-            ]);
+                'help' => 'Наследовать ли условия и ограничения из родительского тарифа'
+            ])
+            ->add('inheritRooms', 'checkbox', [
+                'label' => 'Квоты',
+                'group' => $group,
+                'required' => false,
+                'help' => 'Наследовать ли квоты номеров из родительского тариф'
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => 'MBH\Bundle\PriceBundle\Document\Tariff',
-            'services' => []
+            'data_class' => 'MBH\Bundle\PriceBundle\Document\TariffChildOptions',
+            'parent' => null
         ]);
     }
 
 
     public function getName()
     {
-        return 'mbh_price_tariff_promotions';
+        return 'mbh_price_tariff_child_options';
     }
 
 }
