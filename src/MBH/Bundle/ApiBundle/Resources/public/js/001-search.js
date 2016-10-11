87618b1905a39a4c3af8580e6057bb399285c6db
webmalc <webmalc@gmail.com>
2016-10-11 09:51:21 +0300

    New online form workflow

--- src/MBH/Bundle/ApiBundle/Resources/public/js/001-search.js
+++ src/MBH/Bundle/ApiBundle/Resources/public/js/001-search.js
@@ -2,5 +2,24 @@
 $(document).ready(function () {
     'use strict';
     $('.mbh-search-form select').selectpicker();
-    $('.datepicker').datepicker();
+
+    //begin and end dates
+    (function () {
+        var begin = $('#mbh_api_search_type_begin'),
+            end = $('#mbh_api_search_type_end');
+        end.datepicker({startDate: new Date(Date.now() + 24 * 60 * 60 * 1000)})
+            .inputmask('dd.mm.yyyy', {"placeholder": "ДД.ММ.ГГГГ"});
+        begin.datepicker({startDate: new Date()}).inputmask('dd.mm.yyyy', {"placeholder": "ДД.ММ.ГГГГ"});
+
+        begin.on('change', function () {
+            if (end.datepicker('getDate') && begin.datepicker('getDate') && begin.datepicker('getDate') >= end.datepicker('getDate')) {
+                end.datepicker('setDate', new Date(begin.datepicker('getDate').getTime() + 24 * 60 * 60 * 1000));
+            }
+        });
+        end.on('change', function () {
+            if (end.datepicker('getDate') && begin.datepicker('getDate') && end.datepicker('getDate') <= begin.datepicker('getDate')) {
+                begin.datepicker('setDate', new Date(end.datepicker('getDate').getTime() - 24 * 60 * 60 * 1000));
+            }
+        });
+    }());
 });
