8eccee3414ddb3d7e7b7bf56697c2366d16ead55
Zalex <zalex@zalex.com.ua>
2016-10-13 14:17:00 +0300

    some fixes in twig

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,37 +23,37 @@ echo -e "${GREEN}Start rsync${NC}"
 
 rsync -avz --delete --exclude-from=scripts/$IGNORE * -e ssh $SERVER:$FOLDER
 
-echo -e "${GREEN}Start mbh:demo:apc${NC}"
-ssh $SERVER $APC
-
-echo -e "${GREEN}Make cache, logs and upload directories${NC}"
-ssh $SERVER 'setfacl -R -m u:"www-data":rwX -m u:"root":rwX '$FOLDER'var/cache '$FOLDER'var/logs '$FOLDER'protectedUpload '$FOLDER'web/media'
-ssh $SERVER 'setfacl -dR -m u:"www-data":rwX -m u:"root":rwX '$FOLDER'var/cache '$FOLDER'var/logs '$FOLDER'protectedUpload '$FOLDER'web/media'
-
-echo -e "${GREEN}Start clear:cache${NC}"
-ssh $SERVER $CACHE
-
-echo -e "${GREEN}Start doctrine:mongodb:generate:hydrators${NC}"
-ssh $SERVER $HYDRATORS
-
-echo -e "${GREEN}Start doctrine:mongodb:generate:proxies${NC}"
-ssh $SERVER $PROXIES
-
-echo -e "${GREEN}Start fos:dump${NC}"
-ssh $SERVER $FOS
-
-echo -e "${GREEN}Start assets:install${NC}"
-ssh $SERVER $ASSEST
-
-echo -e "${GREEN}Start assetic:dump${NC}"
-ssh $SERVER $ASSESTIC
-
-echo -e "${GREEN}Start doctrine:mongodb:schema:create${NC}"
-ssh $SERVER $DB
+#echo -e "${GREEN}Start mbh:demo:apc${NC}"
+#ssh $SERVER $APC
+#
+#echo -e "${GREEN}Make cache, logs and upload directories${NC}"
+#ssh $SERVER 'setfacl -R -m u:"www-data":rwX -m u:"root":rwX '$FOLDER'var/cache '$FOLDER'var/logs '$FOLDER'protectedUpload '$FOLDER'web/media'
+#ssh $SERVER 'setfacl -dR -m u:"www-data":rwX -m u:"root":rwX '$FOLDER'var/cache '$FOLDER'var/logs '$FOLDER'protectedUpload '$FOLDER'web/media'
 
 echo -e "${GREEN}Start clear:cache${NC}"
 ssh $SERVER $CACHE
 
-ssh $SERVER $PHP_FPM
-ssh $SERVER 'service nginx restart'
-
+#echo -e "${GREEN}Start doctrine:mongodb:generate:hydrators${NC}"
+#ssh $SERVER $HYDRATORS
+#
+#echo -e "${GREEN}Start doctrine:mongodb:generate:proxies${NC}"
+#ssh $SERVER $PROXIES
+#
+#echo -e "${GREEN}Start fos:dump${NC}"
+#ssh $SERVER $FOS
+#
+#echo -e "${GREEN}Start assets:install${NC}"
+#ssh $SERVER $ASSEST
+#
+#echo -e "${GREEN}Start assetic:dump${NC}"
+#ssh $SERVER $ASSESTIC
+#
+#echo -e "${GREEN}Start doctrine:mongodb:schema:create${NC}"
+#ssh $SERVER $DB
+#
+#echo -e "${GREEN}Start clear:cache${NC}"
+#ssh $SERVER $CACHE
+#
+#ssh $SERVER $PHP_FPM
+#ssh $SERVER 'service nginx restart'
+#
