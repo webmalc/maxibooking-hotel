4b340a5d5b23b723d63b7f45ea8263ff4e1ef9d9
danya <zaluevd@gmail.com>
2016-10-20 17:22:06 +0300

    expedia channel manager init

---
+++ src/MBH/Bundle/ChannelManagerBundle/Document/ExpediaConfig.php
@@ -0,0 +1,311 @@
+<?php
+
+namespace MBH\Bundle\ChannelManagerBundle\Document;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use MBH\Bundle\BaseBundle\Document\Base;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use MBH\Bundle\ChannelManagerBundle\Document\Room;
+use MBH\Bundle\ChannelManagerBundle\Document\Service;
+use MBH\Bundle\ChannelManagerBundle\Document\Tariff;
+use MBH\Bundle\ChannelManagerBundle\Lib\ConfigTrait;
+use MBH\Bundle\ChannelManagerBundle\Lib\CurrencyConfigInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableDocument;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableDocument;
+use MBH\Bundle\BaseBundle\Document\Traits\BlameableDocument;
+use MBH\Bundle\ChannelManagerBundle\Lib\ChannelManagerConfigInterface as BaseInterface;
+use MBH\Bundle\HotelBundle\Document\Hotel;
+use MBH\Bundle\ChannelManagerBundle\Validator\Constraints as MBHValidator;
+
+/**
+ * @ODM\Document(collection="ExpediaConfig")
+ * @Gedmo\Loggable
+ * @MBHValidator\Currency
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt", timeAware=false)
+ */
+class ExpediaConfig extends Base implements BaseInterface, CurrencyConfigInterface
+{
+    public function getName()
+    {
+        return 'expedia';
+    }
+
+    use ConfigTrait;
+
+    /**
+     * Hook timestampable behavior
+     * updates createdAt, updatedAt fields
+     */
+    use TimestampableDocument;
+
+    /**
+     * Hook softdeleteable behavior
+     * deletedAt field
+     */
+    use SoftDeleteableDocument;
+
+    /**
+     * Hook blameable behavior
+     * createdBy&updatedBy fields
+     */
+    use BlameableDocument;
+
+    /**
+     * @Gedmo\Versioned
+     * @ODM\ReferenceOne(targetDocument="MBH\Bundle\HotelBundle\Document\Hotel", inversedBy="expediaConfig")
+     * @Assert\NotNull(message="document.expediaConfig.no_hotel_selected")
+     */
+    protected $hotel;
+
+    /**
+     * @var string
+     * @Gedmo\Versioned
+     * @ODM\Field(type="string")
+     * @Assert\NotNull(message="validator.document.expediaConfig.no_hotel_id_specified")
+     */
+    protected $hotelId;
+
+    /**
+     * @var string
+     * @Gedmo\Versioned
+     * @ODM\Field(type="string")
+     */
+    protected $currency;
+
+    /**
+     * @var float
+     * @Gedmo\Versioned
+     * @ODM\Field(type="float")
+     */
+    protected $currencyDefaultRatio;
+
+    /**
+     * @var array
+     * @ODM\EmbedMany(targetDocument="Room")
+     */
+    protected $rooms;
+
+    /**
+     * @var array
+     * @ODM\EmbedMany(targetDocument="Tariff")
+     */
+    protected $tariffs;
+
+    /**
+     * @var array
+     * @ODM\EmbedMany(targetDocument="Service")
+     */
+    protected $services;
+
+    /**
+     * Set hotel
+     *
+     * @param \MBH\Bundle\HotelBundle\Document\Hotel $hotel
+     * @return self
+     */
+    public function setHotel(Hotel $hotel)
+    {
+        $this->hotel = $hotel;
+        return $this;
+    }
+
+    /**
+     * Get hotel
+     *
+     * @return \MBH\Bundle\HotelBundle\Document\Hotel $hotel
+     */
+    public function getHotel()
+    {
+        return $this->hotel;
+    }
+
+    /**
+     * Set hotelId
+     *
+     * @param string $hotelId
+     * @return self
+     */
+    public function setHotelId($hotelId)
+    {
+        $this->hotelId = $hotelId;
+        return $this;
+    }
+
+    /**
+     * Get hotelId
+     *
+     * @return string $hotelId
+     */
+    public function getHotelId()
+    {
+        return $this->hotelId;
+    }
+
+    public function __construct()
+    {
+        $this->rooms = new ArrayCollection();
+        $this->tariffs = new ArrayCollection();
+        $this->services = new ArrayCollection();
+    }
+
+    /**
+     * Add room
+     *
+     * @param Room $room
+     */
+    public function addRoom(Room $room)
+    {
+        $this->rooms[] = $room;
+    }
+
+    /**
+     * Remove room
+     *
+     * @param Room $room
+     */
+    public function removeRoom(Room $room)
+    {
+        $this->rooms->removeElement($room);
+    }
+
+    /**
+     * Get rooms
+     *
+     * @return \Doctrine\Common\Collections\Collection $rooms
+     */
+    public function getRooms()
+    {
+        return $this->rooms;
+    }
+
+    /**
+     * Add tariff
+     *
+     * @param Tariff $tariff
+     */
+    public function addTariff(Tariff $tariff)
+    {
+        $this->tariffs[] = $tariff;
+    }
+
+    /**
+     * Remove tariff
+     *
+     * @param Tariff $tariff
+     */
+    public function removeTariff(Tariff $tariff)
+    {
+        $this->tariffs->removeElement($tariff);
+    }
+
+    /**
+     * Get tariffs
+     *
+     * @return \Doctrine\Common\Collections\Collection $tariffs
+     */
+    public function getTariffs()
+    {
+        return $this->tariffs;
+    }
+
+    /**
+     * @return $this
+     */
+    public function removeAllRooms()
+    {
+        $this->rooms = new ArrayCollection();
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function removeAllTariffs()
+    {
+        $this->tariffs = new ArrayCollection();
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function removeAllServices()
+    {
+        $this->services = new ArrayCollection();
+
+        return $this;
+    }
+
+    /**
+     * Add service
+     *
+     * @param Service $service
+     */
+    public function addService(Service $service)
+    {
+        $this->services[] = $service;
+    }
+
+    /**
+     * Remove service
+     *
+     * @param Service $service
+     */
+    public function removeService(Service $service)
+    {
+        $this->services->removeElement($service);
+    }
+
+    /**
+     * Get services
+     *
+     * @return \Doctrine\Common\Collections\Collection $services
+     */
+    public function getServices()
+    {
+        return $this->services;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     * @return self
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCurrencyDefaultRatio()
+    {
+        return $this->currencyDefaultRatio;
+    }
+
+    /**
+     * @param float $currencyDefaultRatio
+     * @return self
+     */
+    public function setCurrencyDefaultRatio($currencyDefaultRatio)
+    {
+        $this->currencyDefaultRatio = $currencyDefaultRatio;
+
+        return $this;
+    }
+
+}
---
+++ src/MBH/Bundle/ChannelManagerBundle/Model/RequestInfo.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace MBH\Bundle\ChannelManagerBundle\Model;
+
+class RequestInfo
+{
+    const PUT_METHOD_NAME = 'PUT';
+    const POST_METHOD_NAME = 'POST';
+    const GET_METHOD_NAME = 'GET';
+
+    private $methodName = self::GET_METHOD_NAME;
+    private $requestData = [];
+    private $headersList = [];
+    private $url;
+
+    /**
+     * @param mixed $methodName
+     * @return $this
+     */
+    public function setMethodName($methodName)
+    {
+        $this->methodName = $methodName;
+        return $this;
+    }
+
+    /**
+     * @param $parameterName
+     * @param $data
+     * @return $this
+     */
+    public function addRequestParameter($parameterName, $data)
+    {
+        $this->requestData[$parameterName] = $data;
+        return $this;
+    }
+
+    /**
+     * @param $headerName
+     * @param $data
+     * @return $this
+     */
+    public function addHeader($headerName, $data)
+    {
+        $this->headersList[] = "$headerName: $data";
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethodName()
+    {
+        return $this->methodName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRequestData()
+    {
+        return $this->requestData;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeadersList()
+    {
+        return count($this->headersList) > 0 ? $this->headersList : null;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $url
+     * @return $this
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+}
