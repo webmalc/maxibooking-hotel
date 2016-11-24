5662dfa3f3419ae3c717fbcf3f7c92ff3ced55cd
webmalc <webmalc@gmail.com>
2016-11-24 18:10:16 +0300

    Cache tests

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd docker
 docker-compose up -d
+docker exec -it mbh-php-fpm /bin/bash
--- tests/Bundle/BaseBundle/Service/CacheTest.php
+++ tests/Bundle/BaseBundle/Service/CacheTest.php
@@ -3,20 +3,52 @@
 namespace Tests\Bundle\BaseBundle\Service;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use MBH\Bundle\BaseBundle\Service\Cache;
 
 class CacheTest extends KernelTestCase
 {
-    private $container;
+    const PREFIX = 'tests';
+
+    /**
+     * @var Cache
+     */
+    private $cache;
 
     public function setUp()
     {
         self::bootKernel();
-
-        $this->container = self::$kernel->getContainer();
+        $container = self::$kernel->getContainer();
+        $this->cache = $container->get('mbh.cache');
+        $this->cache->clear();
     }
 
-    public function testAdd()
+    public function testSetAndGet()
     {
-        $this->assertEquals(42, 42);
+        $value = [1 => 'test', 'foo' => 'bar'];
+        $keys = ['one', 'two'];
+        $this->cache->set($value, self::PREFIX, $keys);
+
+        $this->assertEquals($value, $this->cache->get(self::PREFIX, $keys));
+    }
+
+    public function testSetAndGetLongKeyValue()
+    {
+        $value = range(1, 9999);
+        shuffle($value);
+        $keys = range(1, 9999);
+
+        $this->cache->set($value, self::PREFIX, $keys);
+
+        $this->assertEquals($value, $this->cache->get(self::PREFIX, $keys));
+    }
+
+    public function testClear()
+    {
+        $value = [1 => 'test', 'foo' => 'bar'];
+        $keys = ['one', 'two'];
+        $this->cache->set($value, self::PREFIX, $keys);
+        $this->cache->clear();
+
+        $this->assertEquals(false, $this->cache->get(self::PREFIX, $keys));
     }
 }
