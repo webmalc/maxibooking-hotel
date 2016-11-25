98cdfbe3cd3e5cc608c385b2aa5db2929e2b737d
Zalex <zalex@zalex.com.ua>
2016-11-25 17:40:32 +0300

    fix children age to 12 years default && cut above 13 years in list

--- scripts/to_server_deploy.sh
+++ scripts/to_server_deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 rsync -avz --delete --exclude-from=scripts/ignore_new.txt * -e ssh root@mbhs.maxibooking.ru:/var/www/mbhs/deploy/
+ssh root@mbhs.maxibooking.ru setfacl -R -m u:"www-data":rwX /var/www/mbhs/deploy
+ssh root@mbhs.maxibooking.ru setfacl -dR -m u:"www-data":rwX /var/www/mbhs/deploy
