d96c801d20a794473a1f63adb50a30fdadd11b01
crash <crashiura@gmail.com>
2016-12-14 12:57:08 +0300

    update autoUpdate RoomTypeZip

--- src/MBH/Bundle/ClientBundle/Command/UpdateRoomTypeZipCommand.php
+++ src/MBH/Bundle/ClientBundle/Command/UpdateRoomTypeZipCommand.php
@@ -13,7 +13,7 @@ class UpdateRoomTypeZipCommand extends ContainerAwareCommand
      */
     private $dm;
 
-    const MAX = 60 * 1000;
+    const MAX = 60 * 10;
 
     protected function configure()
     {
