94de2009c080f791710b7cf01e2090595589ff89
webmalc <webmalc@gmail.com>
2017-01-16 11:26:34 +0300

    update front controllers

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,13 +1,15 @@
-
 <?php
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+mb_internal_encoding('utf-8');
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
