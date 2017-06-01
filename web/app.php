f22000e01235b5186b1001859a7571134218f24d
Zalex <zalex@zalex.com.ua>
2017-06-01 11:36:02 +0300

    app.php add var  from nginx

--- web/app.php
+++ web/app.php
@@ -8,14 +8,17 @@ $loader = require __DIR__.'/../app/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
-$kernel = new AppKernel('prod', false);
+$request = Request::createFromGlobals();
+$client = $request->server->get('MBCLIENT');
+
+$kernel = new AppKernel('prod', false, $client);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
 //$kernel = new AppCache($kernel);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
