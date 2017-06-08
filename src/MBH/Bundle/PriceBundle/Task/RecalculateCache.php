1dcdabb000178dbbf6667c46c538dfe3e34dd2a5
Zalex <zalex@zalex.com.ua>
2017-06-08 15:16:57 +0300

    fix consumer recalculate cache to console command
    (cherry picked from commit fc15eb2)

--- src/MBH/Bundle/PriceBundle/Task/RecalculateCache.php
+++ src/MBH/Bundle/PriceBundle/Task/RecalculateCache.php
@@ -12,6 +12,10 @@ use PhpAmqpLib\Message\AMQPMessage;
 use MBH\Bundle\PriceBundle\Services\RoomCache;
 use Symfony\Bridge\Monolog\Logger;
 use MBH\Bundle\BaseBundle\Lib\Task\LoggerTrait;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class RecalculateCache implements ConsumerInterface
 {
@@ -22,17 +26,33 @@ class RecalculateCache implements ConsumerInterface
      */
     private $roomCache;
 
-    public function __construct(RoomCache $roomCache, Logger $logger)
+    /** @var KernelInterface */
+    private $kernel;
+
+    public function __construct(RoomCache $roomCache, Logger $logger, KernelInterface $kernel)
     {
         $this->roomCache = $roomCache;
         $this->logger = $logger;
+        $this->kernel = $kernel;
     }
 
     public function execute(AMQPMessage $message)
     {
         $message = unserialize($message->body);
         $this->logStart($message);
-        $this->roomCache->recalculateByPackages($message['begin'], $message['end'], $message['roomTypes']);
+        $application = new Application($this->kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'mbh:cache:recalculate',
+            '--roomTypes' => implode(',',$message['roomTypes']),
+            '--begin' => $message['begin'],
+            '--end' => $message['end']
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
         $this->logCompete();
     }
 }
