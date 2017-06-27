2392d3e5c24c6f2cf5776c06993990a403318b56
danya <faainttt@gmail.com>
2017-06-27 10:51:07 +0300

    color settings update

--- src/MBH/Bundle/ClientBundle/Resources/public/js/003-color_settings.js
+++ src/MBH/Bundle/ClientBundle/Resources/public/js/003-color_settings.js
@@ -3,6 +3,13 @@
  */
 /*global window, $, document, deleteLink, mbh */
 $(document).ready(function () {
-    $('.color-picker').colorpicker();
+    var CPSize = 300;
+    var colorPickerTemplate = '<div class="colorpicker dropdown-menu">'+
+    '<div class="colorpicker-saturation" style="width:'+CPSize+'px;height:'+CPSize+'px"><i><b></b></i></div>'+
+    '<div class="colorpicker-hue" style="height:'+CPSize+'px"><i></i></div>'+
+    '<div class="colorpicker-alpha" style="height:'+CPSize+'px"><i></i></div>'+
+    '<div class="colorpicker-color"><div /></div>'+
+    '</div>';
+    $('.color-picker').colorpicker({template: colorPickerTemplate});
     deleteLink();
 });
