91a646eae526bada72841e3dc7b1b1c6e87fb419
danya <faainttt@gmail.com>
2017-08-03 15:44:54 +0300

    solve charset problem in export to csv

--- src/MBH/Bundle/BaseBundle/Service/Export/CSVExporter.php
+++ src/MBH/Bundle/BaseBundle/Service/Export/CSVExporter.php
@@ -27,6 +27,8 @@ class CSVExporter
     public function exportToCSV(Builder $qb, $entityName, $exportedFields = null): Response
     {
         $rawMongoData = $qb
+            ->limit(0)
+            ->skip(0)
             ->hydrate(false)
             ->getQuery()
             ->execute()
@@ -44,10 +46,10 @@ class CSVExporter
             $rows[] = join(';', $handledEntityData);
         }
         $content = join("\n", $rows);
-
+        $content = iconv('UTF-8', 'windows-1251', $content);
         $response = new Response($content);
         $response->setStatusCode(200);
-        $response->headers->set('Content-Type', 'text/csv; charset=windows-1251');
+        $response->headers->set('Content-Type', 'text/csv');
         $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
 
         return $response;
