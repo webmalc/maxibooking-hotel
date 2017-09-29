aa8a16816fcce269a61b9ae59969a1317ff59a99
danya <faainttt@gmail.com>
2017-09-29 15:35:14 +0300

    fix incorrect day of week

--- src/MBH/Bundle/PackageBundle/Document/PackageRepository.php
+++ src/MBH/Bundle/PackageBundle/Document/PackageRepository.php
@@ -459,7 +459,8 @@ class PackageRepository extends DocumentRepository
         $distributionData = $qb
             ->map(
                 'function() {
-                    emit(this.' . $filterField . '.getDay(), this)
+                    var dayOfWeek = (this.' . $filterField . '.getDay() + 6) % 7;
+                    emit(dayOfWeek, this)
                 }'
             )
             ->reduce(
