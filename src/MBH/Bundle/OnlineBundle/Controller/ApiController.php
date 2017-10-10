637ba76b453927866a4936eb707eb21c370b63fd
danya <faainttt@gmail.com>
2017-10-10 14:13:25 +0300

    add few optional fields to online form

--- src/MBH/Bundle/OnlineBundle/Controller/ApiController.php
+++ src/MBH/Bundle/OnlineBundle/Controller/ApiController.php
@@ -682,7 +682,6 @@ class ApiController extends Controller
                 'accommodation' => false,
                 'tariff' => $info->tariff->id,
                 'isOnline' => true,
-                'accommodation' => false,
             ];
         }
         foreach ($request->services as $info) {
@@ -702,10 +701,14 @@ class ApiController extends Controller
                         'birthday' => $request->user->birthday,
                         'email' => $request->user->email,
                         'phone' => $request->user->phone,
+                        'inn' => $request->user->inn,
+                        'patronymic' => $request->user->patronymic,
+                        'documentNumber' => $request->user->documentNumber
                     ],
                     'status' => 'online',
                     'order_note' => $request->note,
                     'confirmed' => false,
+                    'onlineFormId' => $request->configId
                 ],
                 null,
                 null,
