97466b960a158a92f7efcb636794f6e9becb4f50
Zalex <zalex@zalex.com.ua>
2018-01-18 14:56:17 +0300

    change trusted proxies

--- web/app_dev.php
+++ web/app_dev.php
@@ -5,6 +5,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 mb_internal_encoding('utf-8');
+umask(0000);
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -13,8 +15,8 @@ error_reporting(E_ALL);
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
-Request::setTrustedProxies(['127.0.0.1', '127.0.1.1'], Request::HEADER_X_FORWARDED_ALL);
 $request = Request::createFromGlobals();
+Request::setTrustedProxies(['127.0.0.1', '127.0.1.1', $request->server->get('REMOTE_ADDR')], Request::HEADER_X_FORWARDED_ALL);
 
 //Note! Default client comes  here from NGINX config
 $client = $request->server->get(AppKernel::CLIENT_VARIABLE);
