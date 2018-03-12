dcc1f70b16fa181246454870ea314679b917b114
Zalex <zalex@zalex.com.ua>
2018-03-12 13:22:34 +0300

    fix additional dates show in azovsky.ru

--- src/MBH/Bundle/OnlineBookingBundle/Service/OnlineSearchHelper/DataProviders/AdditionalDataProvider.php
+++ src/MBH/Bundle/OnlineBookingBundle/Service/OnlineSearchHelper/DataProviders/AdditionalDataProvider.php
@@ -7,6 +7,7 @@ namespace MBH\Bundle\OnlineBookingBundle\Service\OnlineSearchHelper\DataProvider
 use MBH\Bundle\OnlineBookingBundle\Lib\OnlineSearchFormData;
 use MBH\Bundle\OnlineBookingBundle\Service\OnlineSearchHelper\ResultCreaters\OnlineCreatorInterface;
 use MBH\Bundle\OnlineBookingBundle\Service\OnlineSearchHelper\SearchQuery\OnlineSearchQueryGenerator;
+use MBH\Bundle\PackageBundle\Lib\SearchResult;
 use MBH\Bundle\PackageBundle\Services\Search\SearchFactory;
 
 class AdditionalDataProvider implements DataProviderInterface
@@ -18,7 +19,7 @@ class AdditionalDataProvider implements DataProviderInterface
     /** @var array */
     private $onlineOptions;
 
-    /** @var OnlineCreatorInterface  */
+    /** @var OnlineCreatorInterface */
     private $onlineResultCreator;
 
     /** @var OnlineSearchQueryGenerator */
@@ -31,7 +32,7 @@ class AdditionalDataProvider implements DataProviderInterface
      * @param OnlineSearchQueryGenerator $queryGenerator
      * @param array $onlineOptions
      */
-    public function __construct(SearchFactory $search,  OnlineCreatorInterface $creator, OnlineSearchQueryGenerator $queryGenerator, array $onlineOptions)
+    public function __construct(SearchFactory $search, OnlineCreatorInterface $creator, OnlineSearchQueryGenerator $queryGenerator, array $onlineOptions)
     {
         $this->search = $search;
         $this->onlineOptions = $onlineOptions;
@@ -46,7 +47,9 @@ class AdditionalDataProvider implements DataProviderInterface
         $range = $this->onlineOptions['add_search_dates'];
         $searchQuery->range = $range;
         $this->search->setAdditionalDates($range);
+        $this->search->setWithTariffs();
         $searchResults = $this->search->search($searchQuery);
+        $searchResults = $this->separateAdditional($searchResults, $formData->getBegin(), $formData->getEnd());
 
         if (count($searchResults)) {
             foreach ($searchResults as $searchResult) {
@@ -59,6 +62,53 @@ class AdditionalDataProvider implements DataProviderInterface
         }
 
         return $results;
+    }
+
+    private function separateAdditional(array $results, \DateTime $begin, \DateTime $end)
+    {
+        $separatedResults = [];
+        /** Filter first */
+        foreach ($results as $result) {
+            $roomType = $result['roomType'];
+            $searchResults = $result['results'];
+            /** Фильтруем точные даты */
+            $searchResults = array_filter($searchResults, function (SearchResult $searchResult) use ($begin, $end){
+                return (bool) !($searchResult->getBegin() == $begin && $searchResult->getEnd() == $end);
+            });
+            $grouped = $this->groupByDay($searchResults);
+            if (count($grouped)) {
+                foreach ($grouped as $value) {
+                    $separatedResults[] = [
+                        'roomType' => $roomType,
+                        'results' => $value
+                    ];
+                }
+            }
+        }
+
+        return $separatedResults;
+    }
+
+    private function groupByDay(array $results): array
+    {
+        $groups = [];
+        foreach ($results as $keyNeedleInstance => $searchNeedleInstance) {
+            /** @var SearchResult $searchNeedleInstance */
+            $needle = $this->getDateHash($searchNeedleInstance);
+            foreach ($results as $searchKey => $searchInstance) {
+                /** @var SearchResult $searchInstance */
+                $hayStack = $this->getDateHash($searchInstance);
+                if ($needle == $hayStack) {
+                    $groups[$needle][$searchKey] = $searchInstance;
+                }
+            }
+        }
+        return $groups;
+    }
+
+    private function getDateHash(SearchResult $searchResult): string
+    {
+        return $searchResult->getBegin()->format('dmY') . $searchResult->getEnd()->format('dmY');
     }
 
     public function getType(): string
