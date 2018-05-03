de22416ca6dda80031348626f07256b5bb4fc9dd
danya <faainttt@gmail.com>
2018-05-03 15:33:56 +0300

    update analytics

--- src/MBH/Bundle/OnlineBundle/Resources/public/js/online/load-results.js
+++ src/MBH/Bundle/OnlineBundle/Resources/public/js/online/load-results.js
@@ -18,33 +18,41 @@ function addLoadEvent(func) {
     }
 }
 addLoadEvent(function() {
-    var yaCounterObjName = 'yaCounter' + yaCounterId;
-    <!-- Yandex.Metrika counter -->
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w[yaCounterObjName] = new Ya.Metrika({
-                    id: yaCounterId,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                });
-            } catch(e) { }
-        });
+    var useYaMetrics = typeof yaCounterId !== 'undefined';
+    if (useYaMetrics) {
+        var yaCounterObjName = 'yaCounter' + yaCounterId;
+        <!-- Yandex.Metrika counter -->
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function () {
+                try {
+                    w[yaCounterObjName] = new Ya.Metrika({
+                        id: yaCounterId,
+                        clickmap: true,
+                        trackLinks: true,
+                        accurateTrackBounce: true,
+                        webvisor: true
+                    });
+                } catch (e) {
+                }
+            });
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () {
+                    n.parentNode.insertBefore(s, n);
+                };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
 
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-    <!-- /Yandex.Metrika counter -->
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else {
+                f();
+            }
+        })(document, window, "yandex_metrika_callbacks");
+        <!-- /Yandex.Metrika counter -->
+    }
 
     var resultsWrapper = document.getElementById('mbh-results-wrapper');
     if (!resultsWrapper) {
@@ -69,7 +77,9 @@ addLoadEvent(function() {
     var processMetricMessage = function (e) {
         if (e.data.type === 'form-event') {
             var purposeType = e.data.purpose;
-            window[yaCounterObjName].reachGoal(purposeType);
+            if (useYaMetrics) {
+                window[yaCounterObjName].reachGoal(purposeType);
+            }
         }
     };
     if (window.addEventListener) {
