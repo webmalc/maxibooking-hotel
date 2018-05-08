c6d6637b4db1cf25a8b7be7e325e7213d24cf3ce
Gosha <--get-all>
2018-05-08 11:04:22 +0300

    #1527 localize

---
+++ src/MBH/Bundle/ClientBundle/Resources/public/js/tinymce/plugin/langs/ru.js
@@ -0,0 +1,17 @@
+tinymce.addI18n('ru', {
+    'MB Entity'          : 'MB данные',
+    'MB Table Entity'    : 'MB табличные данные',
+    'Cash Document'      : 'Документы оплаты',
+    'MB Table properties': 'MB свойства таблицы',
+    'Style table'        : 'Стиль таблицы',
+    'Tourist'            : 'Гости',
+    'Border'             : 'Граница',
+    'Rotate'             : 'Поворот',
+    'User'               : 'Пользователь',
+    'Hotel'              : 'Отель',
+    'Order'              : 'Заказ',
+    'Payer'              : 'Плательщик',
+    'Organization'       : 'Юр. лицо',
+    'Human'              : 'Физ. лицо',
+    'Current date'       : 'Текущая дата'
+});
