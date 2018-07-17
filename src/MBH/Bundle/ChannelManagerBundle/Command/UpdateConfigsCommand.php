ca26695345a74977b8b570da5c85f5b01f15120e
danya <faainttt@gmail.com>
2018-07-17 16:29:11 +0300

    add update configs command

--- src/MBH/Bundle/ChannelManagerBundle/Command/UpdateConfigsCommand.php
+++ src/MBH/Bundle/ChannelManagerBundle/Command/UpdateConfigsCommand.php
@@ -2,6 +2,13 @@
 
 namespace MBH\Bundle\ChannelManagerBundle\Command;
 
+use MBH\Bundle\ChannelManagerBundle\Document\BookingConfig;
+use MBH\Bundle\ChannelManagerBundle\Document\ExpediaConfig;
+use MBH\Bundle\ChannelManagerBundle\Document\HundredOneHotelsConfig;
+use MBH\Bundle\ChannelManagerBundle\Document\MyallocatorConfig;
+use MBH\Bundle\ChannelManagerBundle\Document\OstrovokConfig;
+use MBH\Bundle\ChannelManagerBundle\Document\VashotelConfig;
+use MBH\Bundle\ChannelManagerBundle\Lib\ChannelManagerConfigInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,6 +30,31 @@ class UpdateConfigsCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $dm = $this->getContainer()->get('doctrine.odm.mongodb.document_manager');
 
+        $channelManagerConfigs = [
+            BookingConfig::class,
+            HundredOneHotelsConfig::class,
+            OstrovokConfig::class,
+            VashotelConfig::class,
+            MyallocatorConfig::class,
+            ExpediaConfig::class
+        ];
+
+        foreach ($channelManagerConfigs as $configType) {
+            $configs = $dm->getRepository($configType)->findAll();
+            /** @var ChannelManagerConfigInterface $config */
+            foreach ($configs as $config) {
+                $config->setIsConfirmedWithDataWarnings(true);
+                if (method_exists($config, 'setIsConnectionSettingsRead')) {
+                    $config->setIsConnectionSettingsRead(true);
+                }
+                if (method_exists($config, 'setIsAllPackagesPulled')) {
+                    $config->setIsAllPackagesPulled(true);
+                }
+            }
+        }
+
+        $dm->flush();
     }
 }
