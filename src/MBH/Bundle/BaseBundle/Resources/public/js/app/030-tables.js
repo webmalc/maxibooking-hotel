12b2d196b3440c8871bc4789a5361c390340fb11
kenedias <faainttt@gmail.com>
2018-08-27 09:28:38 +0400

    add export filling table to csv

--- src/MBH/Bundle/BaseBundle/Resources/public/js/app/030-tables.js
+++ src/MBH/Bundle/BaseBundle/Resources/public/js/app/030-tables.js
@@ -299,3 +299,48 @@ function getScrollableTableTemplate($table, tableOffset) {
 
     return templateTable;
 }
+
+jQuery.fn.tableToCSV = function () {
+
+    var clean_text = function (text) {
+        text = text.replace(/"/g, '""').replace(/(\r\n\t|\n|\r\t)/gm,"");
+        return '"' + text + '"';
+    };
+
+    $(this).each(function () {
+        var $table = $(this);
+        var caption = $table.find('caption').text();
+        var title = [];
+        var rows = [];
+
+        $table.find('tr').each(function () {
+            var data = [];
+            $(this).find('th').each(function () {
+                var text = clean_text($(this).text());
+                title.push(text);
+            });
+            $(this).find('td').each(function () {
+                var text = clean_text($(this).text());
+                data.push(text);
+            });
+            data = data.join(";");
+            rows.push(data);
+        });
+        title = title.join(";");
+        rows = rows.join("\n");
+
+        var csv = title + rows;
+        var uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
+        var download_link = document.createElement('a');
+        download_link.href = uri;
+        var ts = moment().format('DD.MM.YYYY HH:mm');
+        if (caption === "") {
+            download_link.download = ts + ".csv";
+        } else {
+            download_link.download = caption + "-" + ts + ".csv";
+        }
+        document.body.appendChild(download_link);
+        download_link.click();
+        document.body.removeChild(download_link);
+    });
+};
