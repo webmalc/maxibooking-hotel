a3aaf16f3816b620b48d7500bfbf14507b2e9b21
kenedias <faainttt@gmail.com>
2018-09-17 20:37:06 +0300

    update endpoint for getting token by user password

--- src/MBH/Bundle/UserBundle/Controller/JsonLoginController.php
+++ src/MBH/Bundle/UserBundle/Controller/JsonLoginController.php
@@ -3,11 +3,13 @@
 namespace MBH\Bundle\UserBundle\Controller;
 
 use MBH\Bundle\BaseBundle\Controller\BaseController;
+use MBH\Bundle\BillingBundle\Lib\Model\Result;
 use MBH\Bundle\UserBundle\Document\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class JsonLoginController extends BaseController
 {
@@ -15,19 +17,43 @@ class JsonLoginController extends BaseController
      * @Route("/user/json_login", name="json_login", requirements={"_format":"json"})
      * @param Request $request
      * @return JsonResponse
+     * @throws \MBH\Bundle\BaseBundle\Lib\Exception
      */
     public function loginAction(Request $request)
     {
+        $result = new Result();
         $this->addAccessControlAllowOriginHeaders($this->getParameter('api_domains'));
         $requestContent = json_decode($request->getContent(), true);
-        /** @var User $user */
-        $user = $this->dm
-            ->getRepository(User::class)
-            ->findOneBy(['username' => $requestContent['username']]);
-        $token = bin2hex(random_bytes(64)) . time();
-        $user->setApiToken($token, new \DateTime('+ 1 month'));
-        $this->dm->flush();
 
-        return new JsonResponse(['status' => 'ok', 'name' => $user->getUsername(), 'token' => $token]);
+        if (empty($requestContent) || !isset($requestContent['username']) || !isset($requestContent['password'])) {
+            $result->setErrors(['Incorrect request content']);
+        } else {
+            $username = $requestContent['username'];
+            $password = $requestContent['password'];
+
+            /** @var User $user */
+            $user = $this->dm
+                ->getRepository(User::class)
+                ->findOneBy(['username' => $username]);
+
+            $encoder = $this
+                ->get('security.encoder_factory')
+                ->getEncoder($user);
+
+            if (is_null($user) || !$encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt())) {
+                $result->setErrors(['Access denied']);
+            } else {
+                $token = bin2hex(random_bytes(64)).time();
+                $user->setApiToken($token, new \DateTime($this->getParameter('token_lifetime_string')));
+                $this->dm->flush();
+
+                $result->setData([
+                    'name' => $user->getUsername(),
+                    'token' => $token
+                ]);
+            }
+        }
+
+        return new JsonResponse($result->getApiResponse());
     }
 }
