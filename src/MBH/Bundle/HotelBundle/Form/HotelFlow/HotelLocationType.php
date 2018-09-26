f86cd07f359cea55c81e9bce985c9ea187642227
kenedias <faainttt@gmail.com>
2018-09-26 14:53:08 +0300

    Merge branch 'master' of bitbucket.org:MaxiBookingTeam/maxibooking-hotel into #2108

--- src/MBH/Bundle/HotelBundle/Form/HotelFlow/HotelLocationType.php
+++ src/MBH/Bundle/HotelBundle/Form/HotelFlow/HotelLocationType.php
@@ -5,12 +5,23 @@ namespace MBH\Bundle\HotelBundle\Form\HotelFlow;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class HotelLocationType extends AbstractType
 {
+    const MAP_URL = 'https://www.google.com/maps/';
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('mapUrl', TextType::class, [
+                'label' => 'form.hotel_contact_information_type.map_url',
+                'required' => false,
+                'group' => 'form.hotelExtendedType.location',
+                'help' => '<a target="_blank" href="' . self::MAP_URL . '">' . self::MAP_URL . '</a>',
+                'constraints' => [new Callback([$this, 'checkMapUrl'])],
+            ])
             ->add('latitude', TextType::class, [
                 'label' => 'form.hotelExtendedType.latitude',
                 'group' => 'form.hotelExtendedType.location',
@@ -25,6 +36,15 @@ class HotelLocationType extends AbstractType
                 'attr' => ['placeholder' => '37.617515'],
                 'help' => 'form.hotelExtendedType.gps_coordinates_longitude<br><a href="#" data-toggle="modal" data-target="#hotel_coordinates_help">form.hotelExtendedType.know_hotel_coordinates</a>'
             ]);
+    }
+
+    public function checkMapUrl($mapUrl, ExecutionContextInterface $context)
+    {
+        //TODO: После мержа с airbnb-веткой, заменить на вызов из сервиса Utils::startsWith
+        $isGoogleMapUrl = substr($mapUrl, 0, strlen(self::MAP_URL)) === self::MAP_URL;
+        if (!$isGoogleMapUrl) {
+            $context->addViolation('validator.hotel.map_url');
+        }
     }
 
     public function getBlockPrefix()
