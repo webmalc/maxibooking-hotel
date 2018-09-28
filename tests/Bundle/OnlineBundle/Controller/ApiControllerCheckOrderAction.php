6fb1c5c284390e52fd2dfe15154fefaab54d8119
Gosha <gosha.senukov@maxi-booking.ru>
2018-09-28 18:57:48 +0300

    #test_for_check_ordre added test for Sberbank

--- tests/Bundle/OnlineBundle/Controller/ApiControllerCheckOrderAction.php
+++ tests/Bundle/OnlineBundle/Controller/ApiControllerCheckOrderAction.php
@@ -7,6 +7,7 @@
 namespace Tests\Bundle\OnlineBundle\Controller;
 
 
+use Doctrine\ODM\MongoDB\DocumentManager;
 use MBH\Bundle\BaseBundle\Lib\Test\Traits\AddPaymentSystemsTrait;
 use MBH\Bundle\BaseBundle\Lib\Test\WebTestCase;
 use MBH\Bundle\CashBundle\Document\CashDocument;
@@ -16,6 +17,7 @@ use MBH\Bundle\ClientBundle\Document\PaymentSystem\NewRbk;
 use MBH\Bundle\ClientBundle\Document\PaymentSystem\Sberbank;
 use MBH\Bundle\ClientBundle\Lib\PaymentSystem\ExtraData;
 use MBH\Bundle\ClientBundle\Lib\PaymentSystem\HolderNamePaymentSystem;
+use MBH\Bundle\ClientBundle\Lib\PaymentSystem\NewRbk\Webhook;
 use MBH\Bundle\ClientBundle\Lib\PaymentSystem\Sberbank\CallbackNotification;
 use MBH\Bundle\PackageBundle\Document\Order;
 use Symfony\Component\HttpFoundation\Request;
@@ -43,8 +45,8 @@ class ApiControllerCheckOrderAction extends WebTestCase
 
     public function getMethodForInvalidStatus(): iterable
     {
-        yield 'GET' => ['GET'];
-        yield 'POST' => ['POST'];
+        yield 'method GET' => ['GET'];
+        yield 'method POST' => ['POST'];
     }
 
     /**
@@ -81,7 +83,7 @@ class ApiControllerCheckOrderAction extends WebTestCase
                 continue;
             }
 
-//            if ($holder->getKey() !== Sberbank::KEY_ARRAY) {
+//            if ($holder->getKey() !== NewRbk::KEY_ARRAY) {
 //                continue;
 //            }
 
@@ -150,7 +152,7 @@ class ApiControllerCheckOrderAction extends WebTestCase
     }
 
     /**
-     * @depends      testSimpleAddPaymentSystems
+     * @depends testSimpleAddPaymentSystems
      */
     public function testValidRequestSberbank()
     {
@@ -163,35 +165,28 @@ class ApiControllerCheckOrderAction extends WebTestCase
         $holder = $this->getExtraData()->getPaymentSystemAsObj(Sberbank::KEY_ARRAY);
 
         /** @var Order $order */
-        $order = $dm->getRepository('MBHPackageBundle:Order')
-            ->findOneBy([
-                'isPaid' => false,
-            ]);
+        $order = $this->getOrder($dm);
 
-        $cashDocument = new CashDocument();
-        $cashDocument->setIsConfirmed(false)
-            ->setIsPaid(false)
-            ->setMethod(CashDocument::METHOD_ELECTRONIC)
-            ->setOperation(CashDocument::OPERATION_IN)
-            ->setOrder($order)
-            ->setTotal($order->getPaid());
-
-        $order->addCashDocument($cashDocument);
-
-        $dm->persist($cashDocument);
-        $dm->flush();
+        /** @var CashDocument $cashDocument */
+        $cashDocument = $this->createCashDocument($dm, $order);
 
         $url = self::PREFIX_URL . $holder->getKey();
 
         $this->client->request('GET', $url, $this->dataSberbank($clientConfig->getSberbank(), $cashDocument));
 
         $dm->refresh($cashDocument);
+        $dm->refresh($order);
 
         $this->assertStatusCodeWithMsg($url, 200);
 
         $this->assertContains(
             \MBH\Bundle\ClientBundle\Service\PaymentSystem\Wrapper\Sberbank::SUCCESSFUL_RESPONSE,
             $this->client->getResponse()->getContent()
+        );
+
+        $this->assertTrue(
+            $order->getIsPaid(),
+            'The status of isPaid in Order has not been changed.'
         );
 
         $this->assertTrue(
@@ -225,6 +220,279 @@ class ApiControllerCheckOrderAction extends WebTestCase
         $fakeRequest = null;
 
         return $param;
+    }
+
+    /**
+     * runInSeparateProcess
+     * @depends testSimpleAddPaymentSystems
+     */
+    public function testValidRequestNewRbk()
+    {
+        $container = $this->getContainer();
+        $dm = $container->get('doctrine.odm.mongodb.document_manager');
+
+        /** @var ClientConfig $clientConfig */
+        $clientConfig = $container->get('mbh.client_config_manager')->fetchConfig();
+
+        $newRbk = $clientConfig->getNewRbk();
+        $newRbk->setWebhookKey($this->getPublicKey());
+
+        $clientConfig->setNewRbk($newRbk);
+
+        /** @var Order $order */
+        $order = $this->getOrder($dm);
+
+        /** @var CashDocument $cashDocument */
+        $cashDocument = $this->createCashDocument($dm, $order);
+
+        $holder = $this->getExtraData()->getPaymentSystemAsObj(NewRbk::KEY_ARRAY);
+
+        $url = self::PREFIX_URL . $holder->getKey();
+
+        $param = json_encode(
+            [
+                "eventID"   => 0,
+                "occuredAt" => (new \DateTime())->format(\DateTime::ATOM),
+                "topic"     => Webhook::INVOICES_TOPIC,
+                "eventType" => Webhook::PAYMENT_CAPTURED,
+                'invoice'   => [
+                    "shopID"      => "e7f690a7-2aec-4166-8bf2-d0ff884fde24",
+                    "dueDate"     => (new \DateTime('+ 1 day'))->format(\DateTime::ATOM),
+                    "amount"      => $cashDocument->getTotal(),
+                    "currency"    => "RUB",
+                    "product"     => "Maxi-Test",
+                    "description" => "Keep.Summer.Safe",
+                    "metadata"    => [
+                        "cashId" => $cashDocument->getId(),
+                    ],
+                ],
+            ],
+            JSON_UNESCAPED_UNICODE
+        );
+
+        $this->generateHeaders($param);
+
+        $this->client->request('POST', $url, [], [], [], $param);
+
+        $dm->refresh($cashDocument);
+        $dm->refresh($order);
+
+        $this->assertStatusCodeWithMsg($url, 200);
+
+        $this->assertTrue(
+            $order->getIsPaid(),
+            'The status of isPaid in Order has not been changed.'
+        );
+
+        $this->assertTrue(
+            $cashDocument->getIsPaid(),
+            'The status of isPaid in cashDocument has not been changed.'
+        );
+    }
+
+    private function getOrder(DocumentManager $dm): Order
+    {
+        /** @var Order $order */
+        $order = $dm->getRepository('MBHPackageBundle:Order')
+            ->findOneBy([
+                'deletedAt' => null,
+                'isEnabled' => true,
+            ]);
+
+        $order->setStatus(Order::ONLINE_STATUS);
+        $order->setIsPaid(false);
+
+        $dm->persist($order);
+        $dm->flush();
+
+        return $order;
+    }
+
+    private function createCashDocument(DocumentManager $dm, Order $order): CashDocument
+    {
+
+        $cashDocument = new CashDocument();
+        $cashDocument->setIsConfirmed(false)
+            ->setIsPaid(false)
+            ->setMethod(CashDocument::METHOD_ELECTRONIC)
+            ->setOperation(CashDocument::OPERATION_IN)
+            ->setOrder($order)
+            ->setTotal($order->getTotalOverwrite());
+
+        $order->addCashDocument($cashDocument);
+
+        $dm->persist($cashDocument);
+        $dm->flush();
+
+        return $cashDocument;
+    }
+
+    /**
+     * Пхпюнит перезаписывает заголовки, поэтому подпись просто записывается в глобальный массив
+     *
+     * @param string $data
+     */
+    private function generateHeaders(string $data): void
+    {
+        $signature = '';
+        openssl_sign($data, $signature, $this->getPrivateKey(), OPENSSL_ALGO_SHA256);
+
+        $headers = [
+            'HTTP_CONTENT_SIGNATURE' => 'alg=RS256; digest=' . base64_encode($signature),
+        ];
+
+        $_SERVER['HTTP_CONTENT_SIGNATURE'] = $headers['HTTP_CONTENT_SIGNATURE'];
+    }
+
+    /**
+     * Публичный ключ для проверки подписи
+     * для теста newRbk
+     *
+     * @return string
+     */
+    private function getPublicKey(): string
+    {
+//        $rec = openssl_pkey_get_public($this->getCertificate());
+//
+//        $keyData = openssl_pkey_get_details($rec);
+//
+//        openssl_free_key($rec);
+//
+//        $publicKey = $keyData['key'];
+
+        /*
+         * Ключ сгенерирован на стоках выше
+         */
+        $publicKey = <<<WEBHOOKEY
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAx/kF48U25gOk/Xg7m5wr
++ttqH7+00085XpymeyPugM5RT0eo1NwVcCML614G6BFagQ0eEWOUd9g1amDYVz2q
+SJqbYp50hqiIpGHXF1unbbf94XPo5b5cDtlqDOkx8Q1zrBi+8PKLOBLJu4aNqVaX
+8JBY6Fby9k1nrE9FBFEZDE7sL1ALuD0zUSbExHaoMHYtduESf9RKKzFHiYQK3lmr
+FdxnAQnhWZasUorqIdKMAB3+p2dDkv9mDVMHyLu/DWQkbXKp6Cxi5NTErHC211C1
+Yuj90CiWFWzzyWS1pi/5V/8tE7kvIG9fLKpyNy76MVyP7qAXoUqi0LqBBJ66fVLC
+U5K/IIP1pdtHZzhk5jBwMlUTBqAdt0YF5BAlTt4G562gyNlbbbYcAdMB6+5ZpZDx
+DZCnBUb/qEjDO0wIJLPwhFohbTV2mIQE/eZGj7bedGBScZipHNCKdh1Jg8x23nMJ
+OVmmhAfrG1kBUUv3f3Sde6G+dYnCX7KQ7xv3ucbR0K4pHlK/DO8d8trxwhWX7WvZ
+5J0YyNalrkM906jgh3u1B419j2LiJxsfyJrhIzjNMdePcvf8hfV+zfJw4aVfG2hZ
+vaTIS3J/fEbbiQA5zbDpiwWXJ9HYP+j+38mr6qBOVh18JJcsqD6g87mAyAcrG/Ai
+vggvs44Bh8jZ7ZJE8zxInhECAwEAAQ==
+-----END PUBLIC KEY-----
+WEBHOOKEY;
+
+        return $publicKey;
+    }
+
+    /**
+     * Ключ генерации подписи
+     * для теста newRbk
+     *
+     * @return string
+     */
+    private function getPrivateKey(): string
+    {
+        $key = <<<PRIVATE_KEY
+-----BEGIN PRIVATE KEY-----
+MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQDH+QXjxTbmA6T9
+eDubnCv622ofv7TTTzlenKZ7I+6AzlFPR6jU3BVwIwvrXgboEVqBDR4RY5R32DVq
+YNhXPapImptinnSGqIikYdcXW6dtt/3hc+jlvlwO2WoM6THxDXOsGL7w8os4Esm7
+ho2pVpfwkFjoVvL2TWesT0UEURkMTuwvUAu4PTNRJsTEdqgwdi124RJ/1EorMUeJ
+hAreWasV3GcBCeFZlqxSiuoh0owAHf6nZ0OS/2YNUwfIu78NZCRtcqnoLGLk1MSs
+cLbXULVi6P3QKJYVbPPJZLWmL/lX/y0TuS8gb18sqnI3LvoxXI/uoBehSqLQuoEE
+nrp9UsJTkr8gg/Wl20dnOGTmMHAyVRMGoB23RgXkECVO3gbnraDI2VttthwB0wHr
+7lmlkPENkKcFRv+oSMM7TAgks/CEWiFtNXaYhAT95kaPtt50YFJxmKkc0Ip2HUmD
+zHbecwk5WaaEB+sbWQFRS/d/dJ17ob51icJfspDvG/e5xtHQrikeUr8M7x3y2vHC
+FZfta9nknRjI1qWuQz3TqOCHe7UHjX2PYuInGx/ImuEjOM0x149y9/yF9X7N8nDh
+pV8baFm9pMhLcn98RtuJADnNsOmLBZcn0dg/6P7fyavqoE5WHXwklyyoPqDzuYDI
+Bysb8CK+CC+zjgGHyNntkkTzPEieEQIDAQABAoICADT9IbhnS9LLaG7Z60Gismy4
+s3hSPkI4HgWaEOtUbCCpixYs8Onmn6+lOcVWlxHrd0X3Cd6lunO/UPgtTWZgqryu
+azpIrqv4AK0+V9aSrN0AAkA5jJ9EP/SXW+ir6lXrsJkLvZzvSEDFibstPmB+16gh
+N83pLFxjynL4Vlt8edyvFRq8ZT6eyPhaA43ju1GMHyP/I/3HbRfg6QnheVfN/u2+
+B3VY/uPUbzk4Ii5wMjTdvdryHA/ZqVsig0+HoGqV/Kkb1zX0fQgR8UOYYnrPoiDh
+iFAeURbAViEplJDC6ZmNvo4ZZgl8OYYGbFl/E0D9PA6JUXml41rX8NQek2AeC+vM
+mKj6gt6QDnZRq+2K6o22KkCFIHNBaKR4cOEHKvf2384uSky6UJZFbnl0sg696OOS
+PFISMhQzcuKu1Jzjyk7AeNUTKDR1R4uRf4W7b3Lf8AAQv4jvv7VzheUKYmcuXpRb
+bsJ/Q9R8ggt+9kqWMGN01mnIaDmxH+aase/gtEXrw7AKJoAAfZlX4l64iboOE5g2
+ByKwGemoNlWLf7dI9MKgc6uSCie4L6kBGJ5P48O67Qy4kZ9LtbeuaSSNZVORkQDH
+Snu1iIErnUdNkaYfNer6xg1ryJRQ1ZBR/q+uMZpCPpYT5LYVsKBHjSBLg7JRLqPP
+gKqc0F1I3l+IgVE3NmwBAoIBAQD+sGRziePFuZTiQFk3MNErbPw3L9B2L53z3g2m
+ubgVmjDmKRKRhfg8FrdQvU0jerpvWGYWdcwuvPt0kduUeVzvVoVK2voOjMJGSWwV
+3F2ctIWJmP3xnfBHOgQAtHc17dqeq3BgZbSeg4BNrWusP7n1IaWFXWbi30NMD4bB
+lV/YTF/2lNNVNpUHc+mki7fGyXtb86jH1+OTs66337AsB0m5WgMgbtKvsqVNEvG/
+ErfAt3LQH3rMoxIwBUTWw93QgxdrcUtcjsaouYH/KFzK17HxyoyBUATNry7XNSCW
+Ym+L5mudanQYOmMFpjPzbdlBjrZi6ybiql8qlaFHeQpQHrexAoIBAQDJAIe2+Njz
+A7BPrrCMCqAZG/ZPy6qUk/BqzQbHvP9zWjiSR4J+V5WD3KC930em/SDQ0gXIUHNi
+p31JxxKpfJmubrg2mI71Xc3BnNwAOHNoj5BbIn46or0oTEjdfJ+oVvlHkXhWjmWx
+DJKQLIM4kFMvC/vB+CYqY7FgKKuB8k/BSfsJyCIWtfSjhvJfhzuh+Tu9CrKwAevL
+DBvGUqyNF0RXXORMvnw3bXliAbzznA5gxn4zYt5TcjkebKFW6FqIO8zQETqjdHzU
+9ujrS7Qqf7pYkVBQAF9KdMsLxmXXyBoMT0h9xXoEwAvZmcb8EI1ZY1bGjAOrgMHw
+y2H6iBLl+kRhAoIBAD4q1koxhUyVMRdM97n2C4ibELFz1WGT2+1T7Wcd/CYhvf/g
+VKz6043vSY3Gt1aKtYlKPZkL5eweqw5YMA381ceCCgUskE8O4rj/YQexA5Slp9bf
+ZlUc5TKtoZ5+bn7WcT+7vzF4ge8TUDUluJuR7pU8Qkfcdam5L5cYTx9fk8abVe09
+hJxPN29bRtOoWxKFybu3LSKiuuUpveMaEcmdecxpgUvgYUMLyzeWATZPnlTHMppE
+pNfgkibwkk4N/03PQd8zB18vZqR2q5mZw5Srs76+Xy+NVa7TLQ7Q5ARxQKMYenox
+KudjERqm6BVqYdaTvEdG+PXo+lPNtFFa5T6LUPECggEAGjhEe9zISCMSC8Lo6su0
+CLJ6FfymWs1VjkkCemmwFPcO7B4B6sM4EMRl/36x1Rmt/y92a6P6+UKJ+GbMi2li
+jruIsi6Cb4V2AqyYkjrK6zfXB9xfBP0XmbshwiqwlQALcoeKYBghMdBHCiGUWHuT
+Lk7s32ekauEoUTfc742RU9B3u9XshHPl3rMKyOVWJRHi9g0ANC9797ezo9JDCgCf
+/Jl0eoJap8xTCjLZ2BliUsNx0PgzskTzKIHZJgIXVTAfzoCA2rOmWQ2AYrk0Xlrd
+DxKdnGIADDFiDz4pKNXEeBibhzSMbzQyF4eUymrKVX+84ReAnw2jvup49HptpKxZ
+4QKCAQEAzbHMYsF216ha6JL0ljogbB+eHikLZuBgkNHd7KlrjgRKpW2srJ9lSoaQ
+wBUf4WLj+6FONrBvF0DPtFAkGCymtNQvcItcvds5aEQ4QlFZ2V+hkWwU7Ls+jVTy
+j/sb1tsfT/y6HEkq6FmdtSaCkD6ieKN4Cpoe+cFxzQ+rwUQ1HrfuoTRDyBITwrul
+BffBFft+WufivSaLmq88Hx6imzpSaeELyo4VtyDyAy4sEwWquZD4opmJz3Uc1Sde
+a0y7Flf7MW6NwGSA77UreHc9HwMWw9RPBCoopPm9Pp8TX6tvhcAEThj0Xmkq3Umm
+WXH3+qEIrVVZTv/G4380e1OPOyyy5Q==
+-----END PRIVATE KEY-----
+PRIVATE_KEY;
+
+        return $key;
+    }
+
+    /**
+     * Сертификат для генерации публичного ключа
+     * для теста newRbk
+     *
+     * @return string
+     */
+    private function getCertificate(): string
+    {
+        $cert = <<<CERT
+-----BEGIN CERTIFICATE-----
+MIIFkjCCA3oCCQCKRGwbsM4GNjANBgkqhkiG9w0BAQsFADCBijELMAkGA1UEBhMC
+UlUxDTALBgNVBAgMBEMxMzcxETAPBgNVBAcMCFNpbiBDaXR5MQ0wCwYDVQQKDARN
+YXhpMQwwCgYDVQQLDANkZXYxDjAMBgNVBAMMBWdvc2hhMSwwKgYJKoZIhvcNAQkB
+Fh1nb3NoYS5zZW51a292QG1heGktYm9va2luZy5ydTAeFw0xODA5MjgxMjAxNTBa
+Fw0yMTAzMTYxMjAxNTBaMIGKMQswCQYDVQQGEwJSVTENMAsGA1UECAwEQzEzNzER
+MA8GA1UEBwwIU2luIENpdHkxDTALBgNVBAoMBE1heGkxDDAKBgNVBAsMA2RldjEO
+MAwGA1UEAwwFZ29zaGExLDAqBgkqhkiG9w0BCQEWHWdvc2hhLnNlbnVrb3ZAbWF4
+aS1ib29raW5nLnJ1MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAx/kF
+48U25gOk/Xg7m5wr+ttqH7+00085XpymeyPugM5RT0eo1NwVcCML614G6BFagQ0e
+EWOUd9g1amDYVz2qSJqbYp50hqiIpGHXF1unbbf94XPo5b5cDtlqDOkx8Q1zrBi+
+8PKLOBLJu4aNqVaX8JBY6Fby9k1nrE9FBFEZDE7sL1ALuD0zUSbExHaoMHYtduES
+f9RKKzFHiYQK3lmrFdxnAQnhWZasUorqIdKMAB3+p2dDkv9mDVMHyLu/DWQkbXKp
+6Cxi5NTErHC211C1Yuj90CiWFWzzyWS1pi/5V/8tE7kvIG9fLKpyNy76MVyP7qAX
+oUqi0LqBBJ66fVLCU5K/IIP1pdtHZzhk5jBwMlUTBqAdt0YF5BAlTt4G562gyNlb
+bbYcAdMB6+5ZpZDxDZCnBUb/qEjDO0wIJLPwhFohbTV2mIQE/eZGj7bedGBScZip
+HNCKdh1Jg8x23nMJOVmmhAfrG1kBUUv3f3Sde6G+dYnCX7KQ7xv3ucbR0K4pHlK/
+DO8d8trxwhWX7WvZ5J0YyNalrkM906jgh3u1B419j2LiJxsfyJrhIzjNMdePcvf8
+hfV+zfJw4aVfG2hZvaTIS3J/fEbbiQA5zbDpiwWXJ9HYP+j+38mr6qBOVh18JJcs
+qD6g87mAyAcrG/Aivggvs44Bh8jZ7ZJE8zxInhECAwEAATANBgkqhkiG9w0BAQsF
+AAOCAgEAoONl+Sk+a7h/WRvE3JSoAhJRIzQsbDQ76Uyxi/oSeLjhsdce1emXVzFQ
+W9oLq3X0J6qs4A+DgHnjqRshcJh1IyKec1+6FzXt1FjB88kqI8LRMmNiIczt5QYy
+hMcaLel0wwNfVKvqV/PJWWq/x/A2dt5ZECccr10qLY2IcfxvjDmGHNl29CoWrFWC
+o4iX8GPoNrKNFIJyFN0hqCOBNJx+5kN5ouKN6TC0brEFdluY1RD4L2ToJtup4N4X
+j+jb6izsPIxPxus3ab1LHledRxJ2iwG3WaFPye1p6k7D4PAvCVV3RpcG9rEbny9c
+tQZn/cacegDs+ihwIBtkvv/orEFaGHD7Hv3+9pHE+zXlYh8Tsj5KUT5CKxZeCuQY
+x46JaHVJ9xUrZ0r0zO8jd2TEnNyrVxHYYX8eVyER/rFA2rw7WQB7rIQ0f2vMpPL6
+UveMyZXMoV22GT0XJk+go14AzV12GgyzqoFjcNzexZAmhLjLiMbxM/40raL3xc30
+ylypRIdM5OcB1u2m+/3hH849/gLaEnqzf1DP8vJN+5MAMv6KMKTYrVz7KBg6fovp
+4AObUPtWpaGuQbwPBkA+kYikXgs9TeGVXc+fjcMcuhm+UYyPea87MjFgsmlU+Bys
+t83Vgz8G2/GdOcV73ztZTwQrZ1xkpGSAkJ5n4cgO+rchLifRiaQ=
+-----END CERTIFICATE-----
+CERT;
+
+        return $cert;
     }
 
     /**
