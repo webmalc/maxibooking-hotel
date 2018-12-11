3fd9fa63498ca9a713872750b4778f08fdfa7ed6
kenedias <faainttt@gmail.com>
2018-12-11 14:12:49 +0300

    fix incompability of for each function for nodelist

--- src/MBH/Bundle/BaseBundle/Resources/public/js/menu/020-behavior_menu.js
+++ src/MBH/Bundle/BaseBundle/Resources/public/js/menu/020-behavior_menu.js
@@ -1,3 +1,7 @@
+if (typeof NodeList !== "undefined" && NodeList.prototype && !NodeList.prototype.forEach) {
+    // Yes, there's really no need for `Object.defineProperty` here
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
 var mbhBehaviorMenu = {
   key: 'mbhBehaviorMenu',
   divSidebarAccordion: null,
