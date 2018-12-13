7fa6804137ba3f2e236c60879d14bddb3f9b8a02
Zalex <zalex@zalex.com.ua>
2018-12-13 14:08:25 +0300

    add cache to alias checker python

--- scripts/checkalias/test_checkalias.py
+++ scripts/checkalias/test_checkalias.py
@@ -59,3 +59,30 @@ def test_wrong_client_from_billing():
     with pytest.raises(ValueError) as e:
         checkalias.get_client_from_billing('wrong_name')
     assert str(e.value) in 'No such client wrong_name in billing'
+
+
+def test_get_client_from_cache():
+    client = checkalias.get_client_from_billing('piterprivet')
+    checkalias.alias_cache = {'piterprivet': client}
+    client = checkalias.get_client_from_cache('piterprivet')
+    assert client == client
+
+
+def test_set_client_to_cache():
+    client = checkalias.get_client_from_billing('piterprivet')
+    checkalias.set_client_to_cache(client)
+    assert checkalias.alias_cache['piterprivet'] == client
+
+
+def test_check():
+    client_name = 'piterprivet'
+    client = checkalias.get_client_from_billing(client_name)
+    result = checkalias.check(client_name)
+    assert result == client_name
+    assert checkalias.alias_cache[client_name] == client
+    client_name2 = 'super-demo'
+    result = checkalias.check(client_name)
+    result2 = checkalias.check(client_name2)
+    assert result == client_name
+    assert result2 == 'online-demo'
+    assert len(checkalias.alias_cache.keys()) == 2
