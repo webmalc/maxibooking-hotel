0770d013ce6b04fc2c2de11a29125b602aba26b9
Gosha <guerosf@yandex.ru>
2019-02-21 11:47:29 +0300

    #2705 change description for invoice

--- src/MBH/Bundle/ClientBundle/Lib/PaymentSystem/NewRbk/InvoiceRequest.php
+++ src/MBH/Bundle/ClientBundle/Lib/PaymentSystem/NewRbk/InvoiceRequest.php
@@ -11,9 +11,7 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use MBH\Bundle\CashBundle\Document\CashDocument;
 use MBH\Bundle\ClientBundle\Document\ClientConfig;
 use MBH\Bundle\ClientBundle\Document\NewRbk;
-use MBH\Bundle\PackageBundle\Document\Order;
 use MBH\Bundle\PackageBundle\Document\Package;
-use MBH\Bundle\PackageBundle\Document\PackageService;
 
 /**
  * Class InvoiceRequest
@@ -23,6 +21,11 @@ use MBH\Bundle\PackageBundle\Document\PackageService;
  */
 class InvoiceRequest extends InvoiceCommon implements \JsonSerializable
 {
+    /**
+     * @var Package
+     */
+    private $package;
+
     /**
      * @param DocumentManager $documentManager
      * @param ClientConfig $config
@@ -42,13 +45,13 @@ class InvoiceRequest extends InvoiceCommon implements \JsonSerializable
         $dueDate = new \DateTime('+' . $newRbk->getLifetimeInvoice() . 'days');
 
         $self = new self();
+        $self->package = $package;
         $self->setShopID($newRbk->getShopId());
         $self->setDueDate($dueDate->format(DATE_ATOM)) ;
         $self->setProduct($cashDocument->getPayer()->getName() . ', ' . $package->getNumberWithPrefix());
         $self->setCurrency($config->getCurrency());
         $self->setCashDocumentId($cashDocument);
-//        $self->setMetadata(['cashId' => $cashDocument->getId()]);
-        $self->setDescription($package);
+        $self->setDescription($cashDocument);
         $self->setAmount($cashDocument->getTotal());
 
         $self->setCart($newRbk->isWithFiscalization(), $cashDocument, $newRbk);
@@ -137,35 +140,15 @@ class InvoiceRequest extends InvoiceCommon implements \JsonSerializable
         $this->product = $product;
     }
 
-    /**
-     * @param string $description
-     */
-    public function setDescription(Package $package): void
+    public function setDescription(CashDocument $cashDocument): void
     {
-        $this->description = function ($location) use ($package) {
-            $begin = $package->getBegin();
-            $end = $package->getEnd();
+        $order = $cashDocument->getOrder();
 
-            $msg = 'Проживание в ';
-            if ($location === 'room') {
-                $msg .= $package->getRoomType()->getName();
-            } else {
-                $msg .= $package->getHotel()->getName();
-            }
-            $msg .= ' c ' . $begin->format('d.m.Y') . ' по ' . $end->format('d.m.Y');
-
-            return $msg;
-        };
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription($location = 'hotel'): string
-    {
-        $desc = $this->description;
-
-        return $desc($location);
+        $this->description = sprintf(
+            'Оплата проживания в пансионате по заказу № %s от %s',
+            $order->getId(),
+            $order->getCreatedAt()->format('d.m.Y')
+        );
     }
 
     /**
@@ -173,11 +156,8 @@ class InvoiceRequest extends InvoiceCommon implements \JsonSerializable
      *
      * @param Cart[] $cart
      */
-//    public function setCart(bool $needCart, Order $order, NewRbk $newRbk): void
     public function setCart(bool $needCart, CashDocument $cashDocument, NewRbk $newRbk): void
     {
-        $order = $cashDocument->getOrder();
-
         $cart = [];
 
         if ($needCart) {
@@ -189,31 +169,13 @@ class InvoiceRequest extends InvoiceCommon implements \JsonSerializable
                 $taxMode = null;
             }
 
-            foreach ($order->getPackages() as $package) {
-                $c = new Cart();
-                $c->setProduct($this->getDescription('room'));
-//                $c->setPrice($package->getOrder()->getPrice()*100);
-                $c->setPrice($cashDocument->getTotal()*100);
-                $c->setTaxMode($taxMode);
-                $c->setCost($c->getPrice()*$c->getQuantity());
+            $c = new Cart();
+            $c->setProduct($this->getDescription());
+            $c->setPrice($cashDocument->getTotal()*100);
+            $c->setTaxMode($taxMode);
+            $c->setCost($c->getPrice()*$c->getQuantity());
 
-                $cart[] = $c;
-
-                /** До выяснения подробностей */
-//                /** @var PackageService $packageService */
-//                foreach ($package->getServices() as $packageService) {
-//                    $quantity = $packageService->getAmount() * $packageService->getNights() * $packageService->getPersons();
-//
-//                    $c = new Cart();
-//                    $c->setProduct($packageService->getService()->getName());
-//                    $c->setPrice($packageService->getPrice()*100);
-//                    $c->setQuantity($quantity);
-//                    $c->setTaxMode($taxMode);
-//                    $c->setCost($c->getPrice()*$c->getQuantity());
-//
-//                    $cart[] = $c;
-//                }
-            }
+            $cart[] = $c;
         }
 
         $this->cart = $cart;
