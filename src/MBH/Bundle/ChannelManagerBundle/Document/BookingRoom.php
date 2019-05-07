0484bf0e72c1f69b29e8268c9727372666ef9658
Igor <igor.volkov@maxi-booking.ru>
2019-05-07 11:34:15 +0300

    booking_room revert

--- src/MBH/Bundle/ChannelManagerBundle/Document/BookingRoom.php
+++ src/MBH/Bundle/ChannelManagerBundle/Document/BookingRoom.php
@@ -9,5 +9,28 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
  */
 class BookingRoom extends Room
 {
+    /**
+     * @var bool
+     * @ODM\Field(type="bool")
+     */
+    private $uploadSinglePrices = true;
 
+    /**
+     * @return bool
+     */
+    public function isUploadSinglePrices(): ?bool
+    {
+        return $this->uploadSinglePrices;
+    }
+
+    /**
+     * @param bool $uploadSinglePrices
+     * @return BookingRoom
+     */
+    public function setUploadSinglePrices(bool $uploadSinglePrices): BookingRoom
+    {
+        $this->uploadSinglePrices = $uploadSinglePrices;
+
+        return $this;
+    }
 }
