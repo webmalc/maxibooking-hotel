c40013b414ce8f937862cfe852dead06f00858a6
Igor <igor.volkov@maxi-booking.ru>
2019-05-24 16:28:45 +0300

    ChannelManagerHelper fix for expedia, test, new composer.json dependency 'Mockery'

--- src/MBH/Bundle/ChannelManagerBundle/Services/ChannelManagerHelper.php
+++ src/MBH/Bundle/ChannelManagerBundle/Services/ChannelManagerHelper.php
@@ -7,10 +7,6 @@ use MBH\Bundle\ChannelManagerBundle\Document\Room;
 
 class ChannelManagerHelper
 {
-    private $isRoomTypesInit = false;
-    private $roomTypes = [];
-    private $isTariffsSyncDataInit = false;
-    private $tariffsSyncData;
 
     /**
      * Ленивая загрузка массива, содержащего данные о синхронизации типов комнат сервиса и отеля
@@ -21,33 +17,28 @@ class ChannelManagerHelper
      */
     public function getRoomTypesSyncData(ChannelManagerConfigInterface $config, $byService = false)
     {
-
-        if (!$this->isRoomTypesInit) {
-
-            foreach ($config->getRooms() as $room) {
-                /** @var Room $room */
-                $roomType = $room->getRoomType();
-                if (empty($room->getRoomId()) || !$roomType->getIsEnabled() || !empty($roomType->getDeletedAt())) {
-                    continue;
-                }
-
-                if ($byService) {
-                    $this->roomTypes[$room->getRoomId()] = [
-                        'syncId' => $room->getRoomId(),
-                        'doc' => $roomType
-                    ];
-                } else {
-                    $this->roomTypes[$roomType->getId()] = [
-                        'syncId' => $room->getRoomId(),
-                        'doc' => $roomType
-                    ];
-                }
+        $roomTypes = [];
+        foreach ($config->getRooms() as $room) {
+            /** @var Room $room */
+            $roomType = $room->getRoomType();
+            if (empty($room->getRoomId()) || !$roomType->getIsEnabled() || !empty($roomType->getDeletedAt())) {
+                continue;
             }
 
-            $this->isRoomTypesInit = true;
+            if ($byService) {
+                $roomTypes[$room->getRoomId()] = [
+                    'syncId' => $room->getRoomId(),
+                    'doc' => $roomType
+                ];
+            } else {
+                $roomTypes[$roomType->getId()] = [
+                    'syncId' => $room->getRoomId(),
+                    'doc' => $roomType
+                ];
+            }
         }
 
-        return $this->roomTypes;
+        return $roomTypes;
     }
 
     /**
@@ -59,32 +50,28 @@ class ChannelManagerHelper
      */
     public function getTariffsSyncData(ChannelManagerConfigInterface $config, $byService = false)
     {
-        if (!$this->isTariffsSyncDataInit) {
+        $tariffs = [];
+        foreach ($config->getTariffs() as $configTariff) {
+            /** @var \MBH\Bundle\ChannelManagerBundle\Document\Tariff $configTariff */
+            $tariff = $configTariff->getTariff();
 
-            foreach ($config->getTariffs() as $configTariff) {
-                /** @var \MBH\Bundle\ChannelManagerBundle\Document\Tariff $configTariff */
-                $tariff = $configTariff->getTariff();
-
-                if ($configTariff->getTariffId() === null || !$tariff->getIsEnabled() || !empty($tariff->getDeletedAt())) {
-                    continue;
-                }
-
-                if ($byService) {
-                    $this->tariffsSyncData[$configTariff->getTariffId()] = [
-                        'syncId' => $configTariff->getTariffId(),
-                        'doc' => $tariff
-                    ];
-                } else {
-                    $this->tariffsSyncData[$tariff->getId()] = [
-                        'syncId' => $configTariff->getTariffId(),
-                        'doc' => $tariff
-                    ];
-                }
+            if ($configTariff->getTariffId() === null || !$tariff->getIsEnabled() || !empty($tariff->getDeletedAt())) {
+                continue;
             }
 
-            $this->isTariffsSyncDataInit = true;
+            if ($byService) {
+                $tariffs[$configTariff->getTariffId()] = [
+                    'syncId' => $configTariff->getTariffId(),
+                    'doc' => $tariff
+                ];
+            } else {
+                $tariffs[$tariff->getId()] = [
+                    'syncId' => $configTariff->getTariffId(),
+                    'doc' => $tariff
+                ];
+            }
         }
 
-        return $this->tariffsSyncData;
+        return $tariffs;
     }
 }
