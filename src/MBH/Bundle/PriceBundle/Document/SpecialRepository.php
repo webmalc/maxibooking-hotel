debb6d69b70e0bdbddb6e72573d5a15e65f98c6a
Zalex <zalex@zalex.com.ua>
2019-07-05 16:54:02 +0300

    7.3 micro fix

--- src/MBH/Bundle/PriceBundle/Document/SpecialRepository.php
+++ src/MBH/Bundle/PriceBundle/Document/SpecialRepository.php
@@ -130,7 +130,8 @@ class SpecialRepository extends DocumentRepository
 
         }
 
-        if (count($filter->getRoomTypes())) {
+        $filteredRoomTypes = $filter->getRoomTypes();
+        if (is_array($filteredRoomTypes) && count($filteredRoomTypes)) {
             $exp = $qb->expr();
             foreach ($filter->getRoomTypes() as $roomType) {
                 $exp->addOr($qb->expr()->field('roomTypes')->includesReferenceTo($roomType));
