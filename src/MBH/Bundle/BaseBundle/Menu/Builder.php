6071f3782386db763cafea55c80e9fdb3329260a
Valeriy <oti.kvm@gmail.com>
2019-08-05 15:16:17 +0300

    Hide facebook button for none MB_User

--- src/MBH/Bundle/BaseBundle/Menu/Builder.php
+++ src/MBH/Bundle/BaseBundle/Menu/Builder.php
@@ -8,6 +8,7 @@ use Knp\Menu\ItemInterface;
 use MBH\Bundle\BaseBundle\Lib\Menu\BadgesHolder;
 use MBH\Bundle\ChannelManagerBundle\Services\Airbnb\Airbnb;
 use MBH\Bundle\HotelBundle\Document\QueryCriteria\TaskQueryCriteria;
+use MBH\Bundle\UserBundle\DataFixtures\MongoDB\UserData;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Builder
@@ -787,9 +788,16 @@ class Builder
             $hundredOneHotel,
             $vashotel,
             $myAllLocator,
-            $facebook
+            $this->isMBUser() ? $facebook : []
         ]));
+    }
 
+    public function isMBUser()
+    {
+        /** @var \MBH\Bundle\UserBundle\Document\User $user */
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+
+        return $user instanceof \MBH\Bundle\UserBundle\Document\User && $user->getUsername() === UserData::MB_USER_USERNAME;
     }
 
     /**
